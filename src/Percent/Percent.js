adc5b55da186b1eba5ee8127e59ddd86875a9f4c
tmerlier <tmerlier@student.42.fr>
2016-09-06 18:55:02 +0200

    Add colors to Percent

--- src/Percent/Percent.js
+++ src/Percent/Percent.js
@@ -3,9 +3,18 @@ import React from 'react'
 const Percent = ({metrics, label, icon}) => {
   const value = metrics.partitions[label].yes
   const percent = value ? Math.floor((value / metrics.totalCount) * 100) : 0
+  const color = () => {
+    if (percent < 25) {
+      return 'red'
+    } else if (percent > 75) {
+      return 'green'
+    } else {
+      return 'yellow'
+    }
+  }
 
   return (
-    <div className="ui statistic">
+    <div className={`ui ${color()} statistic`}>
         <div className="value"><i className={`${icon} icon`}></i> {percent}% </div>
         <div className="label">{label} </div>
       </div>
