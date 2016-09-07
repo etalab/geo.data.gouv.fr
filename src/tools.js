3e4125ac26644fe6f7679d16a146ebeebe116cc7
tmerlier <tmerlier@student.42.fr>
2016-09-07 12:23:35 +0200

    Add tools file

---
+++ src/tools.js
@@ -0,0 +1,9 @@
+export const colors = [
+  {name: 'blue', value: '#2185D0'},
+  {name: 'teal', value: '#00B5AD'},
+  {name: 'green', value: '#21BA45'},
+  {name: 'yellow', value: '#FBBD08'},
+  {name: 'purple', value: '#A333C8'},
+  {name: 'orange', value: '#F2711C'},
+  {name: 'red', value: '#DB2828'}
+]
