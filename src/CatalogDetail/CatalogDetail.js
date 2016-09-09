760cdf094fcaa1d82919d1a381c4a0a14df110e2
tmerlier <tmerlier@student.42.fr>
2016-09-09 18:11:09 +0200

    New style

--- src/CatalogDetail/CatalogDetail.js
+++ src/CatalogDetail/CatalogDetail.js
@@ -4,6 +4,7 @@ import BarChart from '../Charts/BarChart/BarChart'
 import Percent from '../Statistics/Percent/Percent'
 import Organizations from '../Organizations/Organizations'
 import File from '../File/File'
+import './CatalogDetail.css'
 
 class CatalogDetail extends Component {
   constructor(props) {
@@ -42,60 +43,62 @@ class CatalogDetail extends Component {
   render() {
     if (this.state.catalog && this.state.metrics) {
       return (
-        <div className="ui stackable grid container">
+        <div id="catalog-detail">
+          <div className="ui stackable grid container">
 
-          <div className="sixteen wide column"></div>
+            <div className="sixteen wide column"></div>
 
-          <div className="ten wide column">
-            <div className="ui header">Catalog</div>
+            <div className="ten wide column">
+              <div className="ui header">Catalog</div>
+              <div className="ui divider"></div>
+              <div className="ui header">{this.state.catalog.name}</div>
+
+              <div className="ui divided items">
+                {[this.state.catalog.serviceUrl].map( (url, idx) => <File key={idx} url={url} description="Description du fichier téléchargeable." />)}
+              </div>
+
+            </div>
+
+            <div className="six wide column">
+            <div className="ui header">Record Type</div>
             <div className="ui divider"></div>
-            <div className="ui header">{this.state.catalog.name}</div>
-
-            <div className="ui divided items">
-              {[this.state.catalog.serviceUrl].map( (url, idx) => <File key={idx} url={url} description="Description du fichier téléchargeable." />)}
+              <PieChart data={this.state.metrics.partitions.recordType} />
             </div>
 
-          </div>
+            <div className="sixteen wide column"></div>
 
-          <div className="six wide column">
-          <div className="ui header">Record Type</div>
-          <div className="ui divider"></div>
-            <PieChart data={this.state.metrics.partitions.recordType} />
-          </div>
-
-          <div className="sixteen wide column"></div>
-
-          <div className="sixteen wide column">
-            <div className="ui header">Data Type</div>
-            <div className="ui divider"></div>
-          </div>
-
-          <div className="ui equal width center aligned stackable grid">
-            <div className="column">
-              <Percent metrics={this.state.metrics} label="openness" icon="users"/>
+            <div className="sixteen wide column">
+              <div className="ui header">Data Type</div>
+              <div className="ui divider"></div>
             </div>
 
-            <div className="column">
-              <BarChart data={this.state.metrics.partitions.dataType} />
+            <div className="ui equal width center aligned stackable grid">
+              <div className="column">
+                <Percent metrics={this.state.metrics} label="openness" icon="users"/>
+              </div>
+
+              <div className="column">
+                <BarChart data={this.state.metrics.partitions.dataType} />
+              </div>
+
+              <div className="column">
+                <Percent metrics={this.state.metrics} label="download" icon="download" />
+              </div>
             </div>
 
-            <div className="column">
-              <Percent metrics={this.state.metrics} label="download" icon="download" />
-            </div>
-          </div>
-
-          <div className="sixteen wide column">
-            <div className="ui header">Counts</div>
-            <div className="ui divider"></div>
-          </div>
-
-          <div className="two column row">
-            <div className="column">
-              <Organizations label="Organizations" organizations={this.state.metrics.counts.organizations} />
+            <div className="sixteen wide column">
+              <div className="ui header">Counts</div>
+              <div className="ui divider"></div>
             </div>
 
-            <div className="column">
-              <Organizations label="Keywords" organizations={this.state.metrics.counts.keywords} />
+            <div className="two column row">
+              <div className="column">
+                <Organizations label="Organizations" organizations={this.state.metrics.counts.organizations} />
+              </div>
+
+              <div className="column">
+                <Organizations label="Keywords" organizations={this.state.metrics.counts.keywords} />
+              </div>
             </div>
           </div>
         </div>
--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router'
+import Statistics from '../Statistics/Statistics'
 import './Home.css'
 
 class Home extends Component {
@@ -9,14 +10,35 @@ class Home extends Component {
         <div className="ui vertical masthead center aligned segment">
 
           <div className="ui text container">
-            <h1 className="ui header">
-              Home
+            <h1 className="ui inverted header">
+              Inspire
             </h1>
-            <Link to={`/catalogs`}>
-              <div className="ui huge primary button">Catalogs List <i className="right arrow icon"></i></div>
+            <h2 className="ui inverted header">
+              Votre organisation gère des <b>données géographiques</b> avec des outils compatibles Inspire et souhaite les rendre disponibles sans effort sur <a href="http://www.data.gouv.fr/fr/">data.gouv.fr</a>.
+            </h2>
+            <Link to="catalogs">
+              <div className="ui huge teal button">Catalogs List <i className="right arrow icon"></i></div>
             </Link>
           </div>
+        </div>
 
+        <div className="ui vertical stripe quote segment">
+          <div className="ui equal width stackable internally celled grid">
+            <div className="center aligned row">
+              <div className="column">
+                <h3>Catalogs</h3>
+                <Statistics value={15} label="Catalog" />
+              </div>
+              <div className="column">
+                <h3>"I shouldn't have gone with their competitor."</h3>
+                <p>That is what they all say about us</p>
+              </div>
+              <div className="column">
+                <h3>Dataset</h3>
+                <Statistics value={76424} label="Catalog" />
+              </div>
+            </div>
+          </div>
         </div>
 
       </div>
