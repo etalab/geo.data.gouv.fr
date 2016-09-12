f2a4678e93e43811acce4da01707da5bd0941d24
tmerlier <tmerlier@student.42.fr>
2016-09-12 10:06:23 +0200

    Add stats to home page

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -6,36 +6,40 @@ import './Home.css'
 class Home extends Component {
   render() {
     return (
-      <div className="pusher">
-        <div className="ui vertical masthead center aligned segment">
+      <div className="ui vertical masthead center aligned segment">
 
-          <div className="ui text container">
-            <h1 className="ui inverted header">
-              Inspire
-            </h1>
-            <h2 className="ui inverted header">
-              Votre organisation gère des <b>données géographiques</b> avec des outils compatibles Inspire et souhaite les rendre disponibles sans effort sur <a href="http://www.data.gouv.fr/fr/">data.gouv.fr</a>.
-            </h2>
-            <Link to="catalogs">
-              <div className="ui huge teal button">Catalogs List <i className="right arrow icon"></i></div>
-            </Link>
-          </div>
+        <div className="ui text container">
+          <h1 className="ui inverted header">
+            Inspire
+          </h1>
+          <h2 className="ui inverted header">
+            Votre organisation gère des <b>données géographiques</b> avec des outils compatibles Inspire et souhaite les rendre disponibles sans effort sur <a href="http://www.data.gouv.fr/fr/">data.gouv.fr</a>.
+          </h2>
+          <Link to="catalogs">
+            <div className="ui huge twitter button">Catalogs List <i className="right arrow icon"></i></div>
+          </Link>
         </div>
 
-        <div className="ui vertical stripe quote segment">
-          <div className="ui equal width stackable internally celled grid">
+        <div className="ui vertical stripe quote segment container">
+          <div className="ui equal width stackable grid">
             <div className="center aligned row">
               <div className="column">
-                <h3>Catalogs</h3>
-                <Statistics value={15} label="Catalog" />
+                <div className="ui segment">
+                  <h3>Need Attention</h3>
+                  <Statistics value={21} label="Waring" color="red" icon="warning sign" />
+                </div>
               </div>
               <div className="column">
-                <h3>"I shouldn't have gone with their competitor."</h3>
-                <p>That is what they all say about us</p>
+                <div className="ui segment">
+                  <h3>Dataset</h3>
+                  <Statistics value={7624} label="Catalogs" color="green" icon="database"/>
+                </div>
               </div>
               <div className="column">
-                <h3>Dataset</h3>
-                <Statistics value={76424} label="Catalog" />
+                <div className="ui segment">
+                  <h3>Awaiting Publication</h3>
+                  <Statistics value={42} label="Work In Progress" color="yellow" icon="hourglass half"/>
+                </div>
               </div>
             </div>
           </div>
