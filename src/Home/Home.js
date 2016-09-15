e6019a4291839c1c3a262c1c27ffae57df0dfd87
tmerlier <tmerlier@student.42.fr>
2016-09-15 11:36:05 +0200

    Add dynamic metrics in Home Component

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -4,7 +4,33 @@ import Statistics from '../Statistics/Statistics'
 import './Home.css'
 
 class Home extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {data: undefined}
+    this.getData()
+  }
+
+  getData() {
+    if (!this.state.data) {
+      return fetch(`https://inspire.data.gouv.fr/api/datasets/metrics`)
+        .then((response) => response.json())
+        .then((data) => {
+          this.setState({data})
+        })
+        .catch((err) => {
+          console.error(err)
+        })
+      }
+  }
+
   render() {
+    const loader =  <div className="ui active inverted dimmer">
+                      <div className="ui large text loader"></div>
+                    </div>
+    const notMatchingAnymore = this.state.data ? <Statistics value={this.state.data.notMatchingAnymore} label="Waring" color="red" icon="warning sign" /> : loader
+    const published = this.state.data ? <Statistics value={this.state.data.published.public + this.state.data.published.private} label="Catalogs" color="green" icon="database"/> : loader
+    const notPublishedYet = this.state.data ? <Statistics value={this.state.data.notPublishedYet} label="Work In Progress" color="yellow" icon="hourglass half"/> : loader
+
     return (
       <div className="ui vertical masthead center aligned segment">
 
@@ -23,24 +49,28 @@ class Home extends Component {
         <div className="ui vertical stripe quote segment container">
           <div className="ui equal width stackable grid">
             <div className="center aligned row">
+
               <div className="column">
                 <div className="ui segment">
                   <h3>Need Attention</h3>
-                  <Statistics value={21} label="Waring" color="red" icon="warning sign" />
+                  {notMatchingAnymore}
                 </div>
               </div>
+
               <div className="column">
                 <div className="ui segment">
                   <h3>Dataset</h3>
-                  <Statistics value={7624} label="Catalogs" color="green" icon="database"/>
+                  {published}
                 </div>
               </div>
+
               <div className="column">
                 <div className="ui segment">
                   <h3>Awaiting Publication</h3>
-                  <Statistics value={42} label="Work In Progress" color="yellow" icon="hourglass half"/>
+                  {notPublishedYet}
                 </div>
               </div>
+
             </div>
           </div>
         </div>
