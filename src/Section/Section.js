e62ee7a13d7c93ee58561f509ff55b03590b402f
tmerlier <tmerlier@student.42.fr>
2016-09-16 13:37:34 +0200

    Add css

--- src/Section/Section.js
+++ src/Section/Section.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Section = ({title, component}) => {
   return (
-    <div className="sixteen wide column">
+    <div className="section sixteen wide column">
       <div className="ui header">{title}</div>
       <div className="ui divider"></div>
       {component}
