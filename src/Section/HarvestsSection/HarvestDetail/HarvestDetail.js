45779be545bb7dc6ae9a54ff700555038bf20eae
tmerlier <tmerlier@student.42.fr>
2016-09-19 12:02:23 +0200

    Add divider to HarvestDetail

--- src/Section/HarvestsSection/HarvestDetail/HarvestDetail.js
+++ src/Section/HarvestsSection/HarvestDetail/HarvestDetail.js
@@ -2,8 +2,11 @@ import React from 'react'
 
 const HarvestDetail = ({harvest}) => {
   return (
-        <div className="ui header">HarvestDetail</div>
-      )
+    <div className="harvest-detail">
+      <div className="ui divider"></div>
+      <div className="ui header">HarvestDetail</div>
+    </div>
+    )
 }
 
 export default HarvestDetail
