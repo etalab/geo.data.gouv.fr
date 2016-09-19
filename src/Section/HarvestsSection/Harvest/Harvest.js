d81133e6b4758ffb1e9c51a18d9a4d9b4562d673
tmerlier <tmerlier@student.42.fr>
2016-09-19 11:49:51 +0200

    Use Accordion component in HarvestSection

--- src/Section/HarvestsSection/Harvest/Harvest.js
+++ src/Section/HarvestsSection/Harvest/Harvest.js
@@ -3,18 +3,26 @@ import moment from 'moment'
 
 const Harvest = ({harvest}) => {
   const color = harvest.status === "successful" ? "green" : "red"
-  const startDate = new Date(harvest.startAt).getTime()
+  const startDate = new Date(harvest.finishedAt).getTime() // finishedAt must be replace by startAt, still does not exist
   const endDate = new Date(harvest.finishedAt).getTime()
   const hoursDifference = moment(endDate).fromNow()
   const duration = moment(endDate).from(startDate)
 
   return (
-      <tr>
-        <td className="collapsing"><div className={`ui ${color} circular label`}>{harvest.status}</div></td>
-        <td>{harvest.recordsFound}</td>
-        <td className="right aligned collapsing">{hoursDifference || 'N/A' }</td>
-        <td className="right aligned collapsing">{duration || 'N/A' }</td>
-      </tr>
+    <div className="ui equal width grid">
+      <div className="column">
+        <div className={`ui ${color} circular label`}>{harvest.status}</div>
+      </div>
+      <div className="column">
+        <p>{harvest.recordsFound} Records</p>
+      </div>
+      <div className="column">
+        <p>{hoursDifference || 'N/A' }</p>
+      </div>
+      <div className="column">
+        <p>{duration || 'N/A' }</p>
+      </div>
+    </div>
       )
 }
 
