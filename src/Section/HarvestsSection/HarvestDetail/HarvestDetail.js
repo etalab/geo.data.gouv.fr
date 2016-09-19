2717e3915e782a95222ab98836773149b714d349
tmerlier <tmerlier@student.42.fr>
2016-09-19 17:24:57 +0200

    Add logs to HarvestDetail

--- src/Section/HarvestsSection/HarvestDetail/HarvestDetail.js
+++ src/Section/HarvestsSection/HarvestDetail/HarvestDetail.js
@@ -1,11 +1,15 @@
 import React from 'react'
 
-const HarvestDetail = ({harvest}) => {
+const HarvestDetail = ({logs}) => {
   return (
     <div className="harvest-detail">
-      <div className="ui header">HarvestDetail</div>
+      <div className="ui padded vertical segment">
+        <div className="ui header">Logs</div>
+        <div className="ui divider"></div>
+        {logs.map((log, idx) => <p key={idx}>{log}</p>)}
+      </div>
     </div>
-    )
+  )
 }
 
 export default HarvestDetail
--- src/Section/HarvestsSection/HarvestsSection.js
+++ src/Section/HarvestsSection/HarvestsSection.js
@@ -1,26 +1,43 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Harvest from './Harvest/Harvest'
 import HarvestDetail from './HarvestDetail/HarvestDetail'
 import Accordion from '../../Accordion/Accordion'
 
-const HarvestsSection = () => {
-    let harvests = []
+class HarvestsSection extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {harvests: undefined}
+    this.getHarvests()
+  }
 
-    harvests.push(this.props.catalog.lastHarvesting)
-    harvests.push(this.props.catalog.lastHarvesting)
-    harvests.push(this.props.catalog.lastHarvesting)
-    harvests.push(this.props.catalog.lastHarvesting)
+  getHarvests() {
+    if (!this.state.harvests) {
+      return fetch(`https://inspire.data.gouv.fr/api/geogw/services/${this.props.catalog.id}/synchronizations`)
+        .then((response) => response.json())
+        .then((harvests) => {
+          this.setState({harvests})
+        })
+        .catch((err) => {
+          console.error(err)
+        })
+      }
+  }
 
-    return (
+  render() {
+    if (this.state.harvests) {
+      return (
       <div className="ui styled accordion">
-        {harvests.map((harvest , idx) =>
+        {this.state.harvests.map((harvest , idx) =>
             <Accordion
               key={idx}
               title={<Harvest harvest={harvest} />}
-              content={<HarvestDetail harvest={harvest} />} />
+              content={<HarvestDetail logs={harvest.log} />} />
         )}
       </div>
-    )
+    )} else {
+      return <div></div>
+    }
+  }
 }
 
 export default HarvestsSection
