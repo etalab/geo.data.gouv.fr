1644b9e5b418dac7fc007755a017c80d27dc4a60
tmerlier <tmerlier@student.42.fr>
2016-09-20 15:13:48 +0200

    Add LineChart component

--- src/Section/HarvestsSection/HarvestsSection.js
+++ src/Section/HarvestsSection/HarvestsSection.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import Harvest from './Harvest/Harvest'
 import HarvestDetail from './HarvestDetail/HarvestDetail'
 import Accordion from '../../Accordion/Accordion'
+import LineChart from '../../Charts/LineChart/LineChart'
 
 class HarvestsSection extends Component {
   constructor(props) {
@@ -23,16 +24,39 @@ class HarvestsSection extends Component {
       }
   }
 
+  getGraphData() {
+    return this.state.harvests
+      .map(harvest => harvest.status === "successful" ? harvest : undefined)
+      .reduce((data, harvest) => {
+        if (harvest) {
+          const date = new Date(harvest.finished).toLocaleDateString().split('-').reverse().join('/')
+          data[date] = harvest.itemsFound
+        }
+        return data
+      }, [])
+  }
+
   render() {
     if (this.state.harvests) {
+      const dataGraph = this.getGraphData()
       return (
-      <div className="ui styled accordion">
-        {this.state.harvests.map((harvest , idx) =>
-            <Accordion
-              key={idx}
-              title={<Harvest harvest={harvest} />}
-              content={<HarvestDetail logs={harvest.log} />} />
-        )}
+      <div className="ui stackable grid">
+
+        <div className="eight wide column">
+          <div className="ui styled accordion">
+            {this.state.harvests.map((harvest , idx) =>
+                <Accordion
+                  key={idx}
+                  title={<Harvest harvest={harvest} />}
+                  content={<HarvestDetail logs={harvest.log} />} />
+            )}
+          </div>
+        </div>
+
+        <div className="eight wide column">
+          <LineChart data={dataGraph} />
+        </div>
+
       </div>
     )} else {
       return <div></div>
