11cec30a94f134f7442867bbbb33fc3490e6a2d2
tmerlier <tmerlier@student.42.fr>
2016-09-23 11:31:02 +0200

    Change openness users icon by unlock icon

--- src/Section/StatisticsSection/StatisticsSection.js
+++ src/Section/StatisticsSection/StatisticsSection.js
@@ -7,7 +7,7 @@ const StatisticsSection = ({metrics}) => {
     return (
       <div className="ui equal width center aligned stackable grid">
         <div className="eight wide column">
-          <Percent metrics={metrics} label="openness" icon="users" description="Percentage of open source data." />
+          <Percent metrics={metrics} label="openness" icon="unlock alternate icon" description="Percentage of open source data." />
         </div>
 
         <div className="eight wide column">
