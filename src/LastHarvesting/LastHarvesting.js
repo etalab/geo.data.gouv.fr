5366e6ed2cac6714071751286b90e026c62a2848
tmerlier <tmerlier@student.42.fr>
2016-09-26 18:58:40 +0200

    Add the ability to choose on which side will display a ribbon

--- src/LastHarvesting/LastHarvesting.js
+++ src/LastHarvesting/LastHarvesting.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import moment from 'moment'
 
-const LastHarvesting = ({harvest}) => {
+const LastHarvesting = ({harvest, side='left'}) => {
   const color = harvest.status === "successful" ? "green" : "red"
   const date = new Date(harvest.finishedAt).getTime()
   const hoursDifference = moment(date).fromNow();
 
   return (
-    <div className={`ui ${color} left ribbon label`}>
+    <div className={`ui ${color} ${side} ribbon label`}>
       {harvest.status} <br /> {hoursDifference}
     </div>
   )
