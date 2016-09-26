918929061cb70135e9b9578c02441a17bba090f2
tmerlier <tmerlier@student.42.fr>
2016-09-26 18:59:13 +0200

    Add LastHarvesting to CatalogDetail

--- src/Section/CatalogSection/CatalogSection.js
+++ src/Section/CatalogSection/CatalogSection.js
@@ -1,23 +1,23 @@
 import React from 'react'
 import Csw from '../../Csw/Csw'
 import Statistics from '../../Statistics/Statistics'
+import LastHarvesting from '../../LastHarvesting/LastHarvesting'
 
 const CatalogSection = ({catalog}) => {
-  const styles = {
-    table: {
-      display: 'inline-block',
-    },
-  }
-
   return (
-    <div style={styles.table}>
-      <div>
+    <div className="ui equal width stackable grid">
+      <div className="column">
         <h1 className="ui header">{catalog.name}</h1>
           <div className="ui divided items">
             {[catalog.serviceUrl].map( (url, idx) => <Csw key={idx} url={url}/>)}
           </div>
       </div>
-      <Statistics value={catalog.lastHarvesting.recordsFound} size="small" label="Records" />
+      <div className="column">
+        <Statistics value={catalog.lastHarvesting.recordsFound} size="small" label="Records" />
+      </div>
+      <div className="column">
+        <LastHarvesting harvest={catalog.lastHarvesting} side='right'/>
+      </div>
     </div>
       )
 }
