fdea915e7dc4f4e86035616754601c2aacc12afe
tmerlier <tmerlier@student.42.fr>
2016-09-26 19:41:57 +0200

    Add space to Statistics text

--- src/Statistics/Statistics.js
+++ src/Statistics/Statistics.js
@@ -1,11 +1,18 @@
 import React from 'react'
 
 const Statistics = ({value, label, unit="", color="black", icon="", size="", description=""}) => {
+
+  const styles = {
+    label: {
+      paddingTop: 4,
+      paddingBottom: 4,
+    }
+  }
   return (
     <div className={`ui ${size} ${color} statistic`}>
       <div className="value">
         <i className={`${icon} icon`}></i> {value} {unit}</div>
-      <div className="label">{label}</div>
+      <div style={styles.label} className="label">{label}</div>
       <p>{description}</p>
     </div>
   )
