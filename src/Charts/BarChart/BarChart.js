50c39a02aef95f38a40d687ec2737cd02edf1dea
tmerlier <tmerlier@student.42.fr>
2016-09-28 13:38:35 +0200

    New responsive BarChart

--- src/Charts/BarChart/BarChart.js
+++ src/Charts/BarChart/BarChart.js
@@ -1,13 +1,10 @@
 import React, { Component } from 'react'
+import MediaQuery from 'react-responsive'
+import Paper from 'material-ui/Paper'
 import { Bar } from 'react-chartjs'
 import { colors } from '../../tools.js'
 
 class BarChart extends Component {
-  constructor(props) {
-    super(props)
-    this.getData = this.getData.bind(this)
-  }
-
   getData() {
     const data = this.props.data
 
@@ -24,13 +21,24 @@ class BarChart extends Component {
 
   render() {
     const data = this.getData()
-
+    const styles = {
+      chart: {
+        display: '-wekit-inline-box',
+        marginLeft: '-3em',
+      },
+    }
     return (
-      <div className="ui grid container">
-        <div className="column">
-          <Bar data={data} />
-        </div>
-      </div>
+      <Paper zDepth={0}>
+
+        <MediaQuery style={styles.chart} maxWidth={550} >
+          <Bar data={data} width={260} height={180} />
+        </MediaQuery>
+
+        <MediaQuery style={styles.chart} minWidth={551}>
+          <Bar data={data} width={420} height={260} />
+        </MediaQuery>
+
+      </Paper>
       )
   }
 }
