64967e7fecefd44ebc6c172ec4d222d2ee48ae08
tmerlier <tmerlier@student.42.fr>
2016-09-29 17:15:27 +0200

    HarvestDetail front

--- src/Section/HarvestsSection/HarvestDetail/HarvestDetail.js
+++ src/Section/HarvestsSection/HarvestDetail/HarvestDetail.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react'
+import Chip from 'material-ui/Chip'
+import Subheader from 'material-ui/Subheader'
+import moment from 'moment'
 
 class HarvestDetail extends Component {
   constructor(props) {
@@ -35,23 +38,80 @@ class HarvestDetail extends Component {
   }
 
   render() {
-    if (this.state.harvest) {
+    if (this.state.harvest && this.state.catalog) {
+      const date = new Date(this.state.harvest.finished).getTime()
+      const hoursDifference = moment(date).fromNow();
+      const success = this.state.harvest.status === 'successful'
+      const styles = {
+        paper: {
+          padding: '3em',
+        },
+        header: {
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'space-between',
+          flexWrap: 'wrap-reverse',
+        },
+        h1: {
+          padding: '0.1em',
+        },
+        chip: {
+          margin: '2em',
+        },
+        content: {
+          padding: '2em',
+        },
+        li: {
+          margin: '1em',
+        },
+        log: {
+          padding: '1em',
+          backgroundColor: this.context.muiTheme.palette.accent2Color,
+        },
+        results: {
+          marginLeft: '0.5em',
+          fontSize: '1.5em',
+        },
+      };
+
+      const content = (success) => {
+        if (success) {
+          return <ul>
+                  {this.state.harvest.log.map((log, idx) =>
+                    <li key={idx} style={styles.li}>
+                      {log.split(':')[0]}: <span style={styles.results}>{log.split(':')[1]}
+                    </span></li>)}
+                </ul>
+        }
+        return <div style={styles.log}>
+                  {this.state.harvest.log.map((log, idx) => <pre key={idx}><code>{log}</code></pre>)}
+                </div>
+      }
+
       return (
-        <div className="harvest-detail">
+        <div style={styles.paper} className="harvest-detail">
 
-          <h1 className="ui header">Catalog</h1>
-          <div className="ui divider"></div>
-          <h2>{this.state.catalog.name}</h2>
+          <div style={styles.header} >
+            <div>
+              <h1 style={styles.h1} className="ui header">{this.state.catalog.name}</h1>
+              <Subheader>Harvest ID: {this.state.harvest._id}</Subheader>
+            </div>
 
-          <h1 className="ui header">Status</h1>
-          <div className="ui divider"></div>
-          <h2>{this.state.harvest.status}</h2>
-
-        <div className="ui padded vertical segment">
-            <div className="ui header">Logs</div>
-            <div className="ui divider"></div>
-            {this.state.harvest.log.map((log, idx) => <pre key={idx}><code>{log}</code></pre>)}
+            <Chip
+              style={styles.chip}
+              labelColor={this.context.muiTheme.palette.alternateTextColor}
+              backgroundColor={success ? this.context.muiTheme.palette.successColor : this.context.muiTheme.palette.failColor}
+              >
+              {this.state.harvest.status} {hoursDifference}
+            </Chip>
           </div>
+
+          <div style={styles.content}>
+            <div className="ui header">{success ? 'Results' : 'Logs'}</div>
+            <div className="ui divider"></div>
+            {content(success)}
+          </div>
+
         </div>
     )} else {
       return <div></div>
@@ -59,4 +119,7 @@ class HarvestDetail extends Component {
   }
 }
 
+HarvestDetail.contextTypes = {
+  muiTheme: React.PropTypes.object.isRequired,
+}
 export default HarvestDetail
