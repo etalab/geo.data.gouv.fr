d2745a9da13678c76d32faf2c660102057ff5d03
tmerlier <tmerlier@student.42.fr>
2016-09-29 17:14:50 +0200

    Comment CustomTheme with hexa colors value

--- src/CustomTheme.js
+++ src/CustomTheme.js
@@ -21,21 +21,23 @@ exports.default = {
   spacing: _spacing2.default,
   fontFamily: 'Roboto, sans-serif',
   palette: {
-    primary1Color: _colors.cyan500,
-    primary2Color: _colors.cyan700,
-    primary3Color: _colors.grey400,
-    accent1Color: _colors.pinkA200,
-    accent2Color: _colors.grey100,
-    accent3Color: _colors.grey500,
+    primary1Color: _colors.cyan500, //#00BCD4
+    primary2Color: _colors.cyan700, //#0097A7
+    primary3Color: _colors.grey400, //#BDBDBD
+    accent1Color: _colors.pinkA200, //#FF4081
+    accent2Color: _colors.grey100, //#F5F5F5
+    accent3Color: _colors.grey500, //#9E9E9E
     textColor: _colors.darkBlack,
     secondaryTextColor: (0, _colorManipulator.fade)(_colors.darkBlack, 0.54),
-    alternateTextColor: _colors.white,
-    canvasColor: _colors.white,
-    borderColor: _colors.grey300,
+    alternateTextColor: _colors.white, //#ffffff
+    canvasColor: _colors.white, //#ffffff
+    borderColor: _colors.grey300, //#E0E0E0
     disabledColor: (0, _colorManipulator.fade)(_colors.darkBlack, 0.3),
-    pickerHeaderColor: _colors.cyan500,
+    pickerHeaderColor: _colors.cyan500, //#00BCD4
     clockCircleColor: (0, _colorManipulator.fade)(_colors.darkBlack, 0.07),
-    shadowColor: _colors.fullBlack
+    shadowColor: _colors.fullBlack,
+    successColor: _colors.green500, //#4CAF50
+    failColor: _colors.red500, // #F44336
   }
 }; /**
     * NB: If you update this file, please also update `docs/src/app/customization/Themes.js`
