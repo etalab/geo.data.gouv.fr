34646547d502ed2a804122753fd42166e48ec9fb
tmerlier <tmerlier@student.42.fr>
2016-10-03 17:07:47 +0200

    Remove CSW Component

--- src/Section/CatalogSection/CatalogSection.js
+++ src/Section/CatalogSection/CatalogSection.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import Csw from '../../Csw/Csw'
 import Statistics from '../../Statistics/Statistics'
 
 const CatalogSection = ({catalog}) => {
@@ -7,9 +6,7 @@ const CatalogSection = ({catalog}) => {
     <div className="ui equal width stackable grid">
       <div className="column">
         <h1 className="ui header">{catalog.name}</h1>
-          <div className="ui divided items">
-            {[catalog.serviceUrl].map( (url, idx) => <Csw key={idx} url={url}/>)}
-          </div>
+        <a href={catalog.serviceUrl}>Get the CSW stream</a>
       </div>
       <div className="column">
         <Statistics value={catalog.lastHarvesting.recordsFound} size="small" label="Records" />
