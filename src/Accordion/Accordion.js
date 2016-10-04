c71ad5229b6adcf3dad7e06e0308c3cc390d3a3c
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2016-10-04 14:32:27 +0200

    Revert "remove Accordion"
    This reverts commit 6ad66e6e8742ed6f20e33bb396e604386056c91f.

---
+++ src/Accordion/Accordion.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+
+class Accordion extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {active: props.active ? props.active : false}
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick() {
+    this.setState({active: !this.state.active})
+  }
+
+  render() {
+    return (
+      <div>
+        <div className={`${this.state.active ? 'active' : '' } title`} onClick={this.handleClick} >
+          {this.props.title}
+          {!this.state.active && this.props.header ? <div className="ui divider"></div> : undefined}
+        </div>
+        <div className={`${this.state.active ? 'active' : '' } content`}>
+          <div className="ui divider"></div>
+          {this.props.content}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Accordion
