84b46a815cf3f4a4a37399efd73cc9996d8bd391
tmerlier <tmerlier@student.42.fr>
2016-10-05 16:21:46 +0200

    Remove unused stats

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -28,23 +28,9 @@ class Home extends Component {
 
   render() {
     const loader =  <CircularProgress size={1.5} />
-    const notMatchingAnymore = this.state.data ? <Counter value={this.state.data.notMatchingAnymore} label="Warning" color="red" icon="warning sign" /> : loader
     const published = this.state.data ? <Counter value={this.state.data.published.public + this.state.data.published.private} label="Catalogs" color="green" icon="database"/> : loader
-    const notPublishedYet = this.state.data ? <Counter value={this.state.data.notPublishedYet} label="Work In Progress" color="yellow" icon="hourglass half"/> : loader
 
     const styles = {
-      header1: {
-        tablet: {
-          marginTop: '1.5em',
-          marginBottom: '0.5em',
-          fontSize: '2em',
-          fontWeight: 'normal',
-        },
-        marginTop: '0.5em',
-        marginBottom: '0.5em',
-        fontSize: '4em',
-        fontWeight: 'normal',
-      },
       header2: {
         tablet: {
           fontSize: '1.2em',
@@ -82,18 +68,8 @@ class Home extends Component {
         </div>
 
         <Paper style={styles.paper} rounded={true} zDepth={2}>
-          <h3>Need Attention</h3>
-          {notMatchingAnymore}
-        </Paper>
-
-        <Paper style={styles.paper} rounded={true} zDepth={2}>
           <h3>Dataset</h3>
           {published}
-        </Paper>
-
-        <Paper style={styles.paper} rounded={true} zDepth={2}>
-          <h3>Awaiting Publication</h3>
-          {notPublishedYet}
         </Paper>
 
       </div>
