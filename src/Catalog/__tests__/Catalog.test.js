b656cd18b635825eb2e1c10102ac85d3a9251bab
tmerlier <tmerlier@student.42.fr>
2016-10-12 17:27:00 +0200

    Update Catalog tests

--- src/Catalog/__tests__/Catalog.test.js
+++ src/Catalog/__tests__/Catalog.test.js
@@ -1,69 +1,64 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import Percent from '../Statistics/Percent/Percent'
+import Percent from '../../Statistics/Percent/Percent'
 import CircularProgress from 'material-ui/CircularProgress'
-import Counter from '../Statistics/Counter/Counter'
-import Catalog from './Catalog'
+import Counter from '../../Statistics/Counter/Counter'
+import Catalog from '../Catalog'
+import { mountWithContext } from '../../../test/jsdom-setup'
 
-jest.mock('../fetch/fetchMetrics');
+import catalog from './catalog.json'
+import metrics from './metrics.json'
+jest.mock('../../fetch/fetchMetrics');
 
-const goodCatalog = {
-  id: "1",
-  name: "Géocatalogue national",
-  tags: [ ],
-  serviceUrl: "http://www.geocatalogue.fr/api-public/servicesRest",
-  lastHarvesting: {
-  status: "failed",
-  recordsFound: 0,
-  finishedAt: "2016-09-01T14:03:24.339Z"
-  }
-}
+describe('<Catalog />', () => {
 
-const badCatalog = {
-  id: "0",
-  name: "Géocatalogue national",
-  tags: [ ],
-  serviceUrl: "http://www.geocatalogue.fr/api-public/servicesRest",
-  lastHarvesting: {
-  status: "failed",
-  recordsFound: 0,
-  finishedAt: "2016-09-01T14:03:24.339Z"
-  }
-}
+  describe('When all goes well', () => {
+    const wrapper = shallow(<Catalog catalog={catalog} />);
+    it('renders without crashing', () => {
+      shallow(<Catalog catalog={catalog} />);
+    });
 
-describe('Catalog test', () => {
-  it('renders without crashing', () => {
-    shallow(<Catalog catalog={goodCatalog} />);
-  });
+    it('should display the name of the catalog', () => {
+      const title = <span className="ui large header">{catalog.name}</span>
+      expect(wrapper.contains(title)).toEqual(true);
+    });
 
-  it('should display the name of the catalog', () => {
-    const title = <span className="ui large header">{goodCatalog.name}</span>
-    const wrapper = shallow(<Catalog catalog={goodCatalog} />);
-    expect(wrapper.contains(title)).toEqual(true);
-  });
+    it('should display the number of records', () => {
+      const records = <Counter value={0} size="small" label="Records" />
+      expect(wrapper.contains(records)).toEqual(true);
+    });
 
-  it('should display the number of records', () => {
-    const records = <Counter value={0} size="small" label="Records" />
-    const wrapper = shallow(<Catalog catalog={goodCatalog} />);
-    console.log(wrapper.debug());
-    expect(wrapper.contains(records)).toEqual(true);
-  });
+    it('should display the openness percent', () => {
+      const open = <Percent value={808} total={833} label="open data" icon="unlock alternate icon" size="small" />
 
-  it('should display the openness percent', () => {
-    const open = <Percent value={808} total={833} label="open data" icon="unlock alternate icon" size="small" />
-    const wrapper = shallow(<Catalog catalog={goodCatalog} />);
-    expect(wrapper.contains(open)).toEqual(true);
-  });
+      wrapper.setState({metrics: metrics});
 
-  it('should display the downloadable percent', () => {
-    const download = <Percent value={728} total={833} label="downloadable" icon="download" size="small" />
-    const wrapper = shallow(<Catalog catalog={goodCatalog} />);
-    expect(wrapper.contains(download)).toEqual(true);
-  });
+      expect(wrapper.contains(open)).toEqual(true);
+    });
 
-  it('should display a loading when data are undefined yet.', () => {
-    const loader = <CircularProgress size={1} />
-    const wrapper = shallow(<Catalog catalog={badCatalog} />);
-    expect(wrapper.contains(loader)).toEqual(true);
-  });
+    it('should display the downloadable percent', () => {
+      const download = <Percent value={728} total={833} label="downloadable" icon="download" size="small" />
+
+      wrapper.setState({metrics: metrics});
+
+      expect(wrapper.contains(download)).toEqual(true);
+    });
+  })
+
+  describe('fetch metrics', () => {
+    it('should set metrics', () => {
+      const wrapper = mountWithContext(<Catalog catalog={catalog} />)
+      wrapper
+        .instance()
+        .componentWillMount()
+        .then(() => expect(wrapper.state('metrics')).toEqual(metrics))
+    });
+
+    it('should display a loading.', () => {
+      const loader = <CircularProgress size={1} />
+      const wrapper = shallow(<Catalog catalog={catalog} />);
+      expect(wrapper.contains(loader)).toEqual(true);
+    });
+  })
+
 })
