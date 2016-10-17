f230e6e9abeaf8577a81bc47149e7421e21f2fcb
tmerlier <tmerlier@student.42.fr>
2016-10-17 22:30:13 +0200

    HarvestRow is now stateless

--- src/HarvestRow/HarvestRow.js
+++ src/HarvestRow/HarvestRow.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Link } from 'react-router'
 import {TableRow, TableRowColumn} from 'material-ui/Table'
 import MediaQuery from 'react-responsive'
@@ -7,47 +7,48 @@ import RaisedButton from 'material-ui/RaisedButton';
 import IconButton from 'material-ui/IconButton';
 import NavigationMoreHoriz from 'material-ui/svg-icons/navigation/more-horiz'
 
-class HarvestRow extends Component {
-  render() {
-    const color = this.props.harvest.status === "successful" ? "green" : "red"
-    const endDate = new Date(this.props.harvest.finished).getTime()
-    const hoursDifference = moment(endDate).fromNow()
-    const styles = {
-      button: {
-        minWidth: '',
-        fontSize: '2em',
-      },
-    }
+function doneSince(endTime) {
+  const endDate = new Date(endTime).getTime()
+  return moment(endDate).fromNow()
+}
 
-    return (
-      <TableRow hoverable={true}>
-        <TableRowColumn>
-          <MediaQuery style={styles.label} minWidth={651} className={`ui ${color} circular label`} >
-            {this.props.harvest.status}
-          </MediaQuery>
-          <MediaQuery maxWidth={650}>
-            <div className={`ui ${color} circular label`} >
-              {this.props.harvest.status === "successful" ? <i className="checkmark icon"/> : <i className="remove icon"/>}
-            </div>
-          </MediaQuery>
-        </TableRowColumn>
-        <TableRowColumn>{this.props.harvest.itemsFound}</TableRowColumn>
-        <TableRowColumn>{hoursDifference || 'N/A' }</TableRowColumn>
-        <TableRowColumn>
-          <Link to={`/catalogs/${this.props.catalog.id}/harvest/${this.props.harvest._id}`}>
-            <MediaQuery minWidth={651} >
-              <RaisedButton label="More" primary={true} />
-            </MediaQuery>
-            <MediaQuery maxWidth={650} >
-              <IconButton>
-                <NavigationMoreHoriz />
-              </IconButton>
-            </MediaQuery>
-          </Link>
-        </TableRowColumn>
-      </TableRow>
-        )
+const HarvestRow = ({harvest, catalog}) => {
+  const color = harvest.status === "successful" ? "green" : "red"
+  const styles = {
+    button: {
+      minWidth: '',
+      fontSize: '2em',
+    },
   }
+
+  return (
+    <TableRow hoverable={true}>
+      <TableRowColumn>
+        <MediaQuery style={styles.label} minWidth={651} className={`ui ${color} circular label`} >
+          {harvest.status}
+        </MediaQuery>
+        <MediaQuery maxWidth={650}>
+          <div className={`ui ${color} circular label`} >
+            {harvest.status === "successful" ? <i className="checkmark icon"/> : <i className="remove icon"/>}
+          </div>
+        </MediaQuery>
+      </TableRowColumn>
+      <TableRowColumn>{harvest.itemsFound}</TableRowColumn>
+      <TableRowColumn>{doneSince(harvest.finished) || 'N/A' }</TableRowColumn>
+      <TableRowColumn>
+        <Link to={`/catalogs/${catalog.id}/harvest/${harvest._id}`}>
+          <MediaQuery minWidth={651} >
+            <RaisedButton label="More" primary={true} />
+          </MediaQuery>
+          <MediaQuery maxWidth={650} >
+            <IconButton>
+              <NavigationMoreHoriz />
+            </IconButton>
+          </MediaQuery>
+        </Link>
+      </TableRowColumn>
+    </TableRow>
+      )
 }
 
 export default HarvestRow
