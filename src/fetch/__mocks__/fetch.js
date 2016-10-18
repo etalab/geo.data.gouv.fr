c0f499aab2f4fdfe9ccdb4c9367a0961e5a1459a
tmerlier <tmerlier@student.42.fr>
2016-10-18 07:49:36 +0200

    Add mock fetchDatasets

--- src/fetch/__mocks__/fetch.js
+++ src/fetch/__mocks__/fetch.js
@@ -1,5 +1,6 @@
 import catalog from '../__test__/catalog.json'
 import metrics from '../__test__/metrics.json'
+import datasets from '../__test__/datasets.json'
 
 export function fetchMetrics(catalogId) {
   if (!catalogId) return Promise.reject(new Error('catalogId is required'))
@@ -19,6 +20,16 @@ export function fetchCatalog(catalogId) {
       resolve(catalog)
     } else {
       reject(new Error('Catalog not found'));
+    };
+  })
+}
+
+export function fetchDatasets(resolve=true) {
+  return new Promise( (resolve, reject) => {
+    if (resolve) {
+      resolve(datasets)
+    } else {
+      reject(new Error('Datasets not found'));
     };
   })
 }
