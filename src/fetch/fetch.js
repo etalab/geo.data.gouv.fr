d8e02271e67f6b9afaff322b13bdbeac29fe10f9
tmerlier <tmerlier@student.42.fr>
2016-10-19 11:46:36 +0200

    Unify data fetching

--- src/fetch/fetch.js
+++ src/fetch/fetch.js
@@ -1,3 +1,27 @@
+export function cancelAllPromise(component) {
+  return component.cancelablePromises.map( promise => promise.cancel())
+}
+
+export const makeCancelable = (promise) => {
+  let hasCanceled_ = false;
+
+  const wrappedPromise = new Promise((resolve, reject) => {
+    promise.then((val) =>
+      hasCanceled_ ? reject({isCanceled: true}) : resolve(val)
+    );
+    promise.catch((error) =>
+      hasCanceled_ ? reject({isCanceled: true}) : reject(error)
+    );
+  });
+
+  return {
+    promise: wrappedPromise,
+    cancel() {
+      hasCanceled_ = true;
+    },
+  };
+};
+
 function _f(url) {
   return fetch(url)
     .then(response => response.json())
@@ -7,16 +31,45 @@ function _f(url) {
     })
 }
 
-export function fetchMetrics(catalogId) {
-  if (!catalogId) return Promise.reject(new Error('catalogId is required'))
-  return _f(`https://inspire.data.gouv.fr/api/geogw/catalogs/${catalogId}/metrics`)
+function waitDataAndSetState(dataPromise, component, stateName) {
+  const cancelablePromise = makeCancelable(dataPromise
+    .then(data => {
+      const update = {};
+      update[stateName] = data;
+      component.setState(update);
+    })
+    .catch(err => {
+      if (!component.state.errors.includes(err.message)) {
+        const errors = [...component.state.errors, err.message]
+        component.setState({ errors })
+      }
+      throw err
+    })
+  )
+  if (!component.cancelablePromises) component.cancelablePromises = []
+
+  component.cancelablePromises.push(cancelablePromise)
+  return cancelablePromise
 }
 
-export function fetchCatalog(catalogId) {
+export function fetchMetrics(component, catalogId) {
   if (!catalogId) return Promise.reject(new Error('catalogId is required'))
-  return _f(`https://inspire.data.gouv.fr/api/geogw/catalogs/${catalogId}`)
+  const fetchPromise = _f(`https://inspire.data.gouv.fr/api/geogw/catalogs/${catalogId}/metrics`)
+  return waitDataAndSetState(fetchPromise, component, 'metrics')
 }
 
-export function fetchDatasets() {
-  return _f(`https://inspire.data.gouv.fr/api/datasets/metrics`)
+export function fetchCatalog(component, catalogId) {
+  if (!catalogId) return Promise.reject(new Error('catalogId is required'))
+  const fetchPromise = _f(`https://inspire.data.gouv.fr/api/geogw/catalogs/${catalogId}`)
+  return waitDataAndSetState(fetchPromise, component, 'catalog')
+}
+
+export function fetchCatalogs(component) {
+  const fetchPromise = _f('https://inspire.data.gouv.fr/api/geogw/catalogs')
+  return waitDataAndSetState(fetchPromise, component, 'catalogs')
+}
+
+export function fetchDatasets(component) {
+  const fetchPromise = _f('https://inspire.data.gouv.fr/api/datasets/metrics')
+  return waitDataAndSetState(fetchPromise, component, 'datasets')
 }
