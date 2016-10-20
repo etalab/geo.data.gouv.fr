d8abb38c603568e9e367151a5bb1369b0053f059
tmerlier <tmerlier@student.42.fr>
2016-10-20 15:47:32 +0200

    Update fetch.js mock

--- src/fetch/__mocks__/fetch.js
+++ src/fetch/__mocks__/fetch.js
@@ -1,72 +1,39 @@
 import catalog from '../__test__/catalog.json'
+import catalogs from '../__test__/catalogs.json'
 import metrics from '../__test__/metrics.json'
+import harvest from '../__test__/harvest.json'
+import harvests from '../__test__/harvests.json'
 import datasets from '../__test__/datasets.json'
 
-export function cancelAllPromise(component) {
-  component.cancelablePromise.map( promise => promise.cancel())
+export function fetchMetrics(catalogId) {
+  if (!catalogId) return Promise.reject(new Error('catalogId is required'));
+  if (catalogId === '1') return Promise.resolve(metrics)
+  return Promise.reject(new Error('metrics not found'))
 }
 
-export const makeCancelable = (promise) => {
-  let hasCanceled_ = false;
-
-  const wrappedPromise = new Promise((resolve, reject) => {
-    promise.then((val) =>
-      hasCanceled_ ? reject({isCanceled: true}) : resolve(val)
-    );
-    promise.catch((error) =>
-      hasCanceled_ ? reject({isCanceled: true}) : reject(error)
-    );
-  });
-
-  return {
-    promise: wrappedPromise,
-    cancel() {
-      hasCanceled_ = true;
-    },
-  };
-};
-
-function _f(data) {
-  return new Promise( (resolve, reject) => {
-    if (data) {
-      return resolve(data)
-    } else {
-      return reject()
-    }
-  })
+export function fetchCatalog(catalogId) {
+  if (!catalogId) return Promise.reject(new Error('catalogId is required'));
+  if (catalogId === '1') return Promise.resolve(catalog)
+  return Promise.reject(new Error('catalog not found'))
 }
 
-function waitDataAndSetState(dataPromise, component, stateName) {
-  const cancelablePromise = makeCancelable(dataPromise
-    .then(data => {
-      component.setState({stateName});
-    })
-    .catch(err => {
-      if (!component.state.errors.includes(err.message)) {
-        const errors = [...component.state.errors, err.message]
-        component.setState({ errors })
-      }
-      throw err
-    })
-  )
-
-    component.setState({cancelablePromise})
-    return cancelablePromise
+export function fetchCatalogs() {
+  return Promise.resolve(catalogs) // Finding a solution to Promise.reject()
 }
 
-export function fetchMetrics(component, catalogId) {
+export function fetchHarvest(catalogId, harvestId) {
   if (!catalogId) return Promise.reject(new Error('catalogId is required'))
-  const fetchPromise = catalogId === '1' ? _f(metrics) : _f()
-  return waitDataAndSetState(fetchPromise, component, metrics)
+  if (!harvestId) return Promise.reject(new Error('harvestId is required'))
+  if (catalogId === '1') return Promise.resolve(harvest)
+  return Promise.reject(new Error('harvest not found'))
 }
 
-export function fetchCatalog(component, catalogId) {
+export function fetchHarvests(catalogId) {
   if (!catalogId) return Promise.reject(new Error('catalogId is required'))
-  const fetchPromise = catalogId === '1' ? _f(catalog) : _f()
-  return waitDataAndSetState(fetchPromise, component, catalog)
+  if (catalogId === '1') return Promise.resolve(harvests)
+  return Promise.reject(new Error('harvests not found'))
 }
 
-export function fetchDatasets(component) {
-  const fetchPromise = _f(datasets)
-  return waitDataAndSetState(fetchPromise, component, datasets)
+export function fetchDatasets() {
+  return Promise.resolve(datasets) // Finding a solution to Promise.reject()
 }
