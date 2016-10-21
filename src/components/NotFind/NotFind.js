ae23acc49a68f4a90827d4e0a1252345eb9769fd
tmerlier <tmerlier@student.42.fr>
2016-10-21 18:09:42 +0200

    Add style to 404

--- src/components/NotFind/NotFind.js
+++ src/components/NotFind/NotFind.js
@@ -2,9 +2,23 @@ import React, { Component } from 'react'
 
 class NotFind extends Component {
   render() {
+    const styles = {
+      notFind: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        color: '#ffffff',
+        fontSize: '2em',
+        position: 'relative',
+        top: '80px',
+      },
+      header: {
+        fontSize: '4em',
+      }
+    }
     return (
-      <div className="notFind">
-        <h1>404</h1>
+      <div style={styles.notFind} className="notFind">
+        <h1 style={styles.header}>404</h1>
         <p>Page non trouvée</p>
       </div>
     )
