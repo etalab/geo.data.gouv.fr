76b0fd0b7ebbd812f5414b52c12e7a7f051e9b3f
tmerlier <tmerlier@student.42.fr>
2016-10-26 14:15:18 +0200

    move dataset title and data source

--- src/components/Dataset/DatasetSection.js
+++ src/components/Dataset/DatasetSection.js
@@ -18,14 +18,14 @@ const DatasetSection = ({dataset}) => {
   return (
     <div style={styles.resume} className="dataset-section">
       <h1 className="dataset-title">{dataset.metadata.title}</h1>
-      <p>{dataset.metadata.description}</p>
-      <p>Origin of data : {dataset.metadata.lineage}</p>
       <div style={styles.infos} className="infos">
         <div>Type : <span>{type}</span></div>
         <div>Licence : <span>{license}</span></div>
         <div>Last update : <span>{revisionDate}</span></div>
       </div>
       <div>Dataset ID : <span>{dataset.metadata.id}</span></div>
+      <p>{dataset.metadata.description}</p>
+      <p>Origin of data : {dataset.metadata.lineage}</p>
     </div>
       )
 }
