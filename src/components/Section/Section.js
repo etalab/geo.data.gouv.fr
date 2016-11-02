cd6207e0e46acb991fc1a3ec7580f315077b1f78
Florian Delezenne <del.florian@gmail.com>
2016-11-02 21:42:59 +0100

    No more win95

--- src/components/Section/Section.js
+++ src/components/Section/Section.js
@@ -1,18 +1,7 @@
 import React, { Component } from 'react'
 
 class Section extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {open: true}
-    this.handleClick = this.handleClick.bind(this)
-  }
-
-  handleClick() {
-    this.setState({open: !this.state.open})
-  }
-
   render() {
-    const expandIcon = this.state.open ?  <span>v</span> : <span>></span>
     const styles = {
       section: {
         paddingTop: '2em',
@@ -24,17 +13,13 @@ class Section extends Component {
       },
       component: {
         padding: 20,
-        display: this.state.open ? '' : 'none',
-      }
+      },
     }
 
     return (
       <div style={styles.section}>
         <div style={styles.header}>
           <h3>{this.props.title}</h3>
-          <button onClick={this.handleClick}>
-            {expandIcon}
-          </button>
         </div>
         <hr />
         <div style={styles.component}>
