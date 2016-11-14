625c70a5dd80041d3ad6f5e1a6eb9246f8496d7b
tmerlier <tmerlier@student.42.fr>
2016-11-14 17:20:37 +0100

    Use DatasetChecklist component

--- src/components/Dataset/DatasetSection.js
+++ src/components/Dataset/DatasetSection.js
@@ -1,23 +1,27 @@
 import React from 'react'
+import DatasetChecklist from './DatasetChecklist'
 import './DatasetDetail.css'
 import { doneSince } from '../../helpers/doneSince'
 
+const styles = {
+  resume: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  infos: {
+    display: 'flex',
+  },
+}
+
 const DatasetSection = ({dataset}) => {
-  const styles = {
-    resume: {
-      display: 'flex',
-      flexDirection: 'column',
-    },
-    infos: {
-      display: 'flex',
-    },
-  }
   const type = dataset.metadata.type || 'unknown'
   const license = dataset.metadata.license || 'not determined'
   const revisionDate = doneSince(dataset.revisionDate)
+
   return (
     <div style={styles.resume} className="dataset-section">
       <h1 className="dataset-title">{dataset.metadata.title}</h1>
+      <DatasetChecklist license={dataset.metadata.license} distributions={dataset.distributions} organizations={dataset.organizations}/>
       <div style={styles.infos} className="infos">
         <div>Type : <span>{type}</span></div>
         <div>Licence : <span>{license}</span></div>
