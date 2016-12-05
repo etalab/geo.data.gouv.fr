b5b6fb955565e9b8adcececa41a8955e81baf8c7
tmerlier <tmerlier@student.42.fr>
2016-12-05 14:57:23 +0100

    Catalog Filters redirect to /records instead of /datasets fix #134

--- src/components/Section/OrganizationsSection/OrganizationsSection.js
+++ src/components/Section/OrganizationsSection/OrganizationsSection.js
@@ -21,7 +21,7 @@ const OrganizationsSection = ({metrics, catalog}) => {
   const organizations = metrics.records.counts.organizations
   const keywords = metrics.records.counts.keywords
 
-  const goToSearch = (filter) => () => browserHistory.push({ pathname: '/datasets', query: {...filter, catalog: catalog.name} })
+  const goToSearch = (filter) => () => browserHistory.push({ pathname: '/records', query: {...filter, catalog: catalog.name} })
   const sections = [{name: 'organization', title: 'Organisations', filters: organizations}, {name: 'keyword', title: 'Mots-clés', filters: keywords}]
 
   return (
