95ea0bea812fe62d1ec36f3922e2019ab0ad9fb6
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2016-12-09 16:25:01 +0100

    Ensure we have values

--- src/components/Catalog/CatalogPreview/CatalogPreview.js
+++ src/components/Catalog/CatalogPreview/CatalogPreview.js
@@ -13,10 +13,21 @@ const styles = {
 
 const CatalogPreview = ({metrics}) => {
   if (metrics) {
+    let openness = 0
+    let download = 0
+
+    if (metrics.datasets.partitions['openness'] && metrics.datasets.partitions['openness'].yes) {
+      openness = metrics.datasets.partitions['openness'].yes;
+    }
+
+    if (metrics.datasets.partitions['download'] && metrics.datasets.partitions['download'].yes) {
+      download = metrics.datasets.partitions['download'].yes;
+    }
+
     return (
       <div style={styles.container}>
-        <Percent value={metrics.datasets.partitions['openness'] ? metrics.datasets.partitions['openness'].yes : 0} total={metrics.datasets.totalCount} label="Données ouvertes" icon="unlock alternate icon" />
-        <Percent value={metrics.datasets.partitions['download'] ? metrics.datasets.partitions['download'].yes : 0} total={metrics.datasets.totalCount} label="Téléchargeable" icon="download" />
+        <Percent value={openness} total={metrics.datasets.totalCount} label="Données ouvertes" icon="unlock alternate icon" />
+        <Percent value={download} label="Téléchargeable" icon="download" />
         <Counter value={metrics.records.totalCount} label="Enregistrements" />
       </div>
     )
