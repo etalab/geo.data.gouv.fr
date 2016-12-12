0b659c3c57afc9d17e7ba94b4a024751b28280fb
Florian Delezenne <del.florian@gmail.com>
2016-12-12 16:52:27 +0100

    Use css cascade again

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,7 +7,7 @@ import { theme  } from '../../tools'
 import { fetchGlobalMetrics  } from '../../fetch/fetch'
 import { waitForDataAndSetState, cancelAllPromises } from '../../helpers/components'
 
-import { masthead, datasetsButton, recordsButton, catalogLinks, datagouv, datagouvIntro, datagouvLink, stats, paper } from './Home.css'
+import { masthead, buttons, inverted, catalogLinks, datagouv, paper } from './Home.css'
 
 const datagouvCounter = {
   value: {
@@ -41,24 +41,22 @@ class Home extends Component {
       <div>
         <div className={masthead}>
           <SearchInput ref="searchInput" handleTextChange={(textInput) => this.userSearch('datasets', textInput)} />
-          <div>
-            <button className={datasetsButton} onClick={() => this.userSearch('datasets', this.refs.searchInput.state.textInput)}>Rechercher un jeu de données</button>
-            <button className={recordsButton} onClick={() => this.userSearch('records', this.refs.searchInput.state.textInput)}>Rechercher un enregistrement</button>
+          <div className={buttons}>
+            <button onClick={() => this.userSearch('datasets', this.refs.searchInput.state.textInput)}>Rechercher un jeu de données</button>
+            <button className={inverted} onClick={() => this.userSearch('records', this.refs.searchInput.state.textInput)}>Rechercher un enregistrement</button>
           </div>
-          <span style={{lineHeight: '4em'}}>OU</span>
+          <span>OU</span>
           <Link className={catalogLinks} to="catalogs">Explorer les 106 catalogues</Link>
         </div>
 
         <div className={datagouv}>
-          <p className={datagouvIntro}>
-            Votre organisation gère des <b>données géographiques</b> avec des outils compatibles Inspire et souhaite les rendre disponibles sans effort sur <a className={datagouvLink} href="http://www.data.gouv.fr/fr/">data.gouv.fr</a>
+          <p>
+            Votre organisation gère des <b>données géographiques</b> avec des outils compatibles Inspire et souhaite les rendre disponibles sans effort sur <a href="http://www.data.gouv.fr/fr/">data.gouv.fr</a>
           </p>
 
-          <div className={stats}>
-            <div className={paper}>
-              <h3>Jeux de données publiés</h3>
-              {published}
-            </div>
+          <div className={paper}>
+            <h3>Jeux de données publiés</h3>
+            {published}
           </div>
         </div>
       </div>
