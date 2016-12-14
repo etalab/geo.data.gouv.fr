39ab4b26ac5440070c66bc49061741cf06f970b9
Florian Delezenne <del.florian@gmail.com>
2016-12-14 16:10:47 +0100

    Modify catalog

--- src/components/Catalog/Catalog.js
+++ src/components/Catalog/Catalog.js
@@ -2,37 +2,13 @@ import React from 'react'
 import { Link } from 'react-router'
 import CatalogPreview from './CatalogPreview/CatalogPreview'
 import LastHarvestStatus from '../LastHarvestStatus/LastHarvestStatus'
-import { theme } from '../../tools'
-
-const styles = {
-  link: {
-    cursor: 'pointer',
-    margin: '1em 3em',
-    display: 'inline-block',
-    width: '300px',
-    position: 'relative',
-  },
-  title: {
-    fontSize: '1.4em',
-  },
-  paper: {
-    backgroundColor: 'white',
-    padding: '2em 20px',
-    boxShadow: theme.boxShadowZ1,
-  },
-  lastHarvesting: {
-    fontSize: '0.8em',
-  },
-  catalogPreview: {
-    marginTop: '1em',
-  },
-}
+import { link, paper, title } from './Catalog.css'
 
 const Catalog = ({ catalog }) => {
   return (
-      <Link to={`/catalogs/${catalog._id}`} style={styles.link}>
-        <div style={styles.paper}>
-          <div style={styles.title}>{catalog.name}</div>
+      <Link to={`/catalogs/${catalog._id}`} className={link}>
+        <div className={paper}>
+          <div className={title}>{catalog.name}</div>
           <LastHarvestStatus harvest={catalog.service.sync}/>
           <CatalogPreview metrics={catalog.metrics} />
         </div>
