a2393877c73febaa9e0214ff8b2084ed9d8e64c6
Florian Delezenne <del.florian@gmail.com>
2016-12-15 11:25:56 +0100

    Modify CatalogPreview

--- src/components/Catalog/CatalogPreview/CatalogPreview.js
+++ src/components/Catalog/CatalogPreview/CatalogPreview.js
@@ -2,22 +2,14 @@ import React from 'react'
 import Counter from '../../Statistics/Counter/Counter'
 import Percent from '../../Statistics/Percent/Percent'
 import { get } from 'lodash'
-
-const styles = {
-  container: {
-    display: 'flex',
-    justifyContent: 'space-between',
-    marginTop: '1em',
-    fontSize: '11px',
-  },
-}
+import { container } from './CatalogPreview.css'
 
 const CatalogPreview = ({metrics}) => {
   let openness = get(metrics, 'datasets.partitions.openness.yes', 0)
   let download = get(metrics, 'datasets.partitions.download.yes', 0)
 
   return (
-    <div style={styles.container}>
+    <div className={container}>
       <Percent value={openness} total={metrics.datasets.totalCount} label="Données ouvertes" icon="unlock alternate icon" />
       <Percent value={download} total={metrics.datasets.totalCount} label="Téléchargeable" icon="download" />
       <Counter value={metrics.records.totalCount} label="Enregistrements" />
