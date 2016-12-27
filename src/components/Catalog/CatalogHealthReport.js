cc25020484408a85d807a83b82309a5fa3b56987
tmerlier <tmerlier@student.42.fr>
2016-12-27 15:49:01 +0100

    WIP

---
+++ src/components/Catalog/CatalogHealthReport.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class CatalogHealthReport extends Component {
+  render() {
+    return (
+      <div><i className="checkmark icon"></i>Tout va bien</div>
+    )
+  }
+}
+
+export default CatalogHealthReport
--- src/components/Section/CatalogSection/CatalogSection.js
+++ src/components/Section/CatalogSection/CatalogSection.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import CatalogHealthReport from '../../Catalog/CatalogHealthReport'
 import { container } from './CatalogSection.css'
 
 const CatalogSection = ({catalog}) => {
   return (
     <div className={container}>
-      <h1>{catalog.name}</h1>
-      <a href={catalog.service.location}>Accès direct au service du catalogue</a>
+      <div>
+        <h1>{catalog.name}</h1>
+        <a href={catalog.service.location}>Accès direct au service du catalogue</a>
+      </div>
+      <CatalogHealthReport catalog={catalog}/>
     </div>
   )
 }
