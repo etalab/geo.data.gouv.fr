f3f0c7a4cf895ab81fb428638d80fe0f519e7fcc
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2016-12-28 16:13:38 +0100

    Fix linting

--- src/components/Organization/__test__/OrganizationCard.test.js
+++ src/components/Organization/__test__/OrganizationCard.test.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import OrganizationCard from '../OrganizationCard'
-import Errors from '../../Errors/Errors'
-import OrganizationMetrics from '../OrganizationMetrics'
 import { shallow } from 'enzyme'
 
 import organizationDetail from '../../../fetch/__test__/organization.json'
