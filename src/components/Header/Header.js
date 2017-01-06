fb6f277741f40184217059726f1ee7b85eab5da8
tmerlier <tmerlier@student.42.fr>
2017-01-06 02:05:21 +0100

    add Logout

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,50 @@
-import React from 'react'
-import { nav, home, login } from './Header.css'
+import React, { Component } from 'react'
+import { Link } from 'react-router'
+import { getUser } from '../../fetch/fetch'
+import { waitForDataAndSetState, cancelAllPromises } from '../../helpers/components'
+import { nav, home, log, account, authentification, avatar } from './Header.css'
 
-const Header = () => {
-  const redirect = encodeURI(`${process.env.PUBLIC_URL}/publication`)
-  const logInUrl = `https://inspire.data.gouv.fr/dgv/login?redirect=${redirect}`
+class Header extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      errors: [],
+    }
+  }
 
-  return (
-    <nav className={nav} role="navigation">
-      <a className={home} href='/'>Inspire</a>
-      <a className={login} href={logInUrl}>Publier des données</a>
-    </nav>
-  )
+  componentWillMount() {
+    return waitForDataAndSetState(getUser(), this, 'user')
+  }
+
+  componentWillUnmount() {
+    return cancelAllPromises(this)
+  }
+
+  render() {
+    const { user } = this.state
+    const loginRedirect = encodeURI(`${process.env.PUBLIC_URL}/publication`)
+    const logoutRedirect = encodeURI(`${process.env.PUBLIC_URL}`)
+    const logInUrl = `https://inspire.data.gouv.fr/dgv/login?redirect=${loginRedirect}`
+    const logoutUrl = `https://inspire.data.gouv.fr/dgv/logout?redirect=${logoutRedirect}`
+    const login = <a className={log} href={logInUrl}>Publier des données</a>
+
+    return (
+      <nav className={nav} role="navigation">
+        <a className={home} href='/'>Inspire</a>
+        {!user ? login : (
+          <div className={authentification}>
+            <Link to={'/publication'}>
+              <div className={account}>
+                <img alt="avatar" className={avatar} src={user.avatar} />
+                {user.first_name} {user.last_name}
+              </div>
+            </Link>
+            <a className={log} href={logoutUrl}>Déconnexion<i className="power icon"></i></a>
+          </div>
+        )}
+      </nav>
+    )
+  }
 }
 
 export default Header
