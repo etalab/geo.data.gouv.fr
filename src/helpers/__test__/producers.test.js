6f1a28726ba4f0e72acb8d234073d0b4beab5182
tmerlier <tmerlier@student.42.fr>
2017-01-10 11:49:58 +0100

    fix json files

---
+++ src/helpers/__test__/producers.test.js
@@ -0,0 +1,25 @@
+import { getUnrelated, getRelatedToOther } from '../producers'
+
+describe('producers', function () {
+
+  describe('getUnrelated(producers)', function () {
+    it('should return an array of unrelated producers', () => {
+      const relatedProducer = {_id: 'relatedProducer', associatedTo: '1'}
+      const unrelatedProducer = {_id: 'unrelatedProducer'}
+      const producers = [relatedProducer, unrelatedProducer]
+
+      expect(getUnrelated(producers)).to.eql([unrelatedProducer])
+    })
+
+    describe('getRelatedToOther(producers, organizationId)', () => {
+      it('should return an array of producers related to others organizations', () => {
+        const relatedToOtherProducer = {_id: 'relatedProducer', associatedTo: 42}
+        const relatedProducer = {_id: 'relatedProducer', associatedTo: 1}
+        const unrelatedProducer = {_id: 'unrelatedProducer'}
+        const producers = [relatedProducer, unrelatedProducer]
+
+        expect(getRelatedToOther(producers, 1)).to.eql([relatedToOtherProducer])
+      })
+    })
+  })
+})
