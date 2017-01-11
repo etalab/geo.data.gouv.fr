30a9349b7f2f8ad86fc5977784120550f4efaf61
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2017-01-11 17:51:17 +0100

    Fix prod build

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -29,7 +29,7 @@ var publicPath = ensureSlash(homepagePathname, true);
 // `publicUrl` is just like `publicPath`, but we will provide it to our app
 // as %PUBLIC_URL% in `index.html` and `process.env.PUBLIC_URL` in JavaScript.
 // Omit trailing shlash as %PUBLIC_PATH%/xyz looks better than %PUBLIC_PATH%xyz.
-var publicUrl = ensureSlash(homepagePathname, false);
+var publicUrl = ensureSlash(homepagePath, false);
 // Get enrivonment variables to inject into our app.
 var env = getClientEnvironment(publicUrl);
 
