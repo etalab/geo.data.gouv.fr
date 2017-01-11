652d91a89e18f808765f333ed964da612ddb47b0
Théophile Merlière <tmerlier@users.noreply.github.com>
2017-01-11 22:20:16 +0100

    Add producers list to OrganizationProducersPreview (#185)

--- src/modules/Publication/components/OrganizationProducersPreview/OrganizationProducersPreview.js
+++ src/modules/Publication/components/OrganizationProducersPreview/OrganizationProducersPreview.js
@@ -6,6 +6,9 @@ const OrganizationProducersPreview = ({ organizationId, producers }) => {
   return (
     <div>
       <div><strong>{producers.length}</strong> producteurs sont associés à votre organisation</div>
+      <ul>
+        {producers.map((producer, idx) => <li key={idx}>{producer._id}</li>)}
+      </ul>
       <Link className={link} to={`/publication/${organizationId}/producers`}>Associer des producteurs</Link>
     </div>
   )
