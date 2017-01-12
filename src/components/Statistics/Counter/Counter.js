7341c30128ed5064669e208eaaed4eb64d14b0bc
tmerlier <tmerlier@student.42.fr>
2017-01-12 09:55:47 +0100

    Fix #178

--- src/components/Statistics/Counter/Counter.js
+++ src/components/Statistics/Counter/Counter.js
@@ -1,20 +1,27 @@
 import React from 'react'
 import cx from 'classnames'
-import { container, defaultLabel, largeLabel, successLabel, warningLabel, errorLabel } from './Counter.css'
+import { container, defaultLabel, large, medium, small, success, warning, error } from './Counter.css'
 
 
 const Counter = ({value, label, unit='', size='', color='', icon='', title=''}) => {
-  const labelStyle = cx(defaultLabel, {
-    [largeLabel]: size === 'large',
-    [successLabel]: color === 'success',
-    [warningLabel]: color === 'warning',
-    [errorLabel]: color === 'error',
+  const labelSize = cx(medium, {
+    [large]: size === 'large',
+    [small]: size === 'small',
   });
 
-  const labelDiv = label ? <div className={labelStyle}>{label}</div> : null
+  const labelColor = cx(medium, {
+    [success]: color === 'success',
+    [warning]: color === 'warning',
+    [error]: color === 'error',
+  });
+
+  console.log(size === 'small')
+  const labelStyle = size === 'small' ? small : defaultLabel
+
   const titleDiv = title ? <h3>{title}</h3> : null
   const iconDiv = icon ? <i className={`${icon} icon`}></i> : null
-  const valueDiv = <div className={labelStyle}>{iconDiv} {value ? value : '0'} {unit}</div>
+  const valueDiv = <div className={`${labelColor} ${labelSize}`}>{iconDiv} {value ? value : '0'} {unit}</div>
+  const labelDiv = label ? <div className={labelStyle}>{label}</div> : null
 
   return (
     <div className={container}>
--- src/modules/Catalogs/components/StatisticsSection/StatisticsSection.js
+++ src/modules/Catalogs/components/StatisticsSection/StatisticsSection.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { get } from 'lodash'
 
 import Chart from '../../../../components/Charts/Chart'
 import DoughnutChart from '../../../../components/Charts/DoughnutChart/DoughnutChart'
@@ -8,36 +9,39 @@ import Percent from '../../../../components/Statistics/Percent/Percent'
 import { container, section, chart } from './StatisticsSection.css'
 
 const StatisticsSection = ({metrics}) => {
-return (
-  <div className={container}>
-    <h2>Indicateurs concernant la totalité du catalogue</h2>
-    <div className={section}>
+  const openness = get(metrics, 'datasets.partitions.openness.yes', 0)
+  const download = get(metrics, 'datasets.partitions.download.yes', 0)
 
-      <div className={chart}>
-        <Counter size="large" value={metrics.records.totalCount} title="Enregistrements"/>
-      </div>
-
-      <div className={chart}>
-        <Chart
-          description={'Répartition des types d\'enregistrements'}
-          chart={<DoughnutChart data={metrics.records.partitions.recordType} />} />
-      </div>
-
-      <div className={chart}>
-        <Chart
-          description={'Répartition des types de meta donnée'}
-          chart={<DoughnutChart data={metrics.records.partitions.metadataType} />} />
-      </div>
-    </div>
-
-    <h2>Indicateurs concernant les jeux de données</h2>
+  return (
+    <div className={container}>
+      <h2>Indicateurs concernant la totalité du catalogue</h2>
       <div className={section}>
+
         <div className={chart}>
-          <Percent value={metrics.datasets.partitions['openness'] ? metrics.datasets.partitions['openness'].yes : 0} total={metrics.datasets.totalCount} size="large" icon="unlock alternate icon" title="Pourcentage de données ouvertes" />
+          <Counter size="large" value={metrics.records.totalCount} title="Enregistrements"/>
         </div>
 
         <div className={chart}>
-          <Percent value={metrics.datasets.partitions['download'] ? metrics.datasets.partitions['download'].yes : 0} total={metrics.datasets.totalCount} size="large" icon="download" title="Pourcentage de jeu de données téléchargeable" />
+          <Chart
+            description={'Répartition des types d\'enregistrements'}
+            chart={<DoughnutChart data={metrics.records.partitions.recordType} />} />
+        </div>
+
+        <div className={chart}>
+          <Chart
+            description={'Répartition des types de meta donnée'}
+            chart={<DoughnutChart data={metrics.records.partitions.metadataType} />} />
+        </div>
+      </div>
+
+      <h2>Indicateurs concernant les jeux de données</h2>
+      <div className={section}>
+        <div className={chart}>
+          <Percent value={openness} total={metrics.datasets.totalCount} size="large" icon="unlock alternate icon" title="Pourcentage de données ouvertes" />
+        </div>
+
+        <div className={chart}>
+          <Percent value={download} total={metrics.datasets.totalCount} size="large" icon="download" title="Pourcentage de jeu de données téléchargeable" />
         </div>
 
         <div className={chart}>
