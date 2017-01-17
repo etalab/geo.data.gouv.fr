a5a03944ab4454aa3fac8acdf8d10e2a0ad2db3e
Florian Delezenne <del.florian@gmail.com>
2017-01-17 13:32:11 +0100

    Use href if there is no name

--- src/modules/Datasets/components/LinksSection/LinksSection.js
+++ src/modules/Datasets/components/LinksSection/LinksSection.js
@@ -2,12 +2,15 @@ import React from 'react'
 import { linkList } from './LinksSection.css'
 
 const LinksSection = ({links, style}) => {
+  // Some links have no name, use href as fallback
+  const getName = (link) => link.name ? link.name : link.href;
+
   return (
     <div>
       <h3>Liens</h3>
       {links.length ? (
         <ul className={linkList}>
-          {links.map( (link, idx) => <li key={idx}><a style={style} href={link.href}>{link.name}</a></li>)}
+          {links.map( (link, idx) => <li key={idx}><a style={style} href={link.href}>{getName(link)}</a></li>)}
         </ul> ) :
         <div>Aucun lien disponible</div>
       }
