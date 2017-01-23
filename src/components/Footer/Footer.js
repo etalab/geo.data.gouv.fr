64da395e1a8d87e8aa97e54df07cd137e6e49ee3
tmerlier <tmerlier@student.42.fr>
2017-01-23 16:38:42 +0100

    Add workshop sign in into footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 
+import EventBriteWidget from '../Workshop/EventBriteWidget'
 import NewsletterForm from '../Newsletter/NewsletterForm'
 
 import { footer } from './Footer.css'
@@ -8,6 +9,8 @@ class Footer extends Component {
   render() {
     return (
       <footer className={footer} >
+        <h2>Actualités</h2>
+        <EventBriteWidget />
         <NewsletterForm />
         <p>Fait avec <span style={{ color: 'white' }}>♥</span> par <a style={{ color: 'white' }} href="https://beta.gouv.fr">l'Incubateur de Services Numériques</a></p>
       </footer>
