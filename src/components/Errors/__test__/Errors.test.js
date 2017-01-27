e72e1a3967cac43ddc48de5b8fbea518fbe5db87
tmerlier <tmerlier@student.42.fr>
2017-01-27 10:57:26 +0100

    Update Errors test

--- src/components/Errors/__test__/Errors.test.js
+++ src/components/Errors/__test__/Errors.test.js
@@ -8,7 +8,7 @@ describe('<Errors />', () => {
 
     it('should display a sentence in the singular', () => {
       const error = ['une erreur']
-      const title = <h3>Une erreur est survenue :</h3>
+      const title = <h3>Une erreur est survenue</h3>
       const wrapper = shallow(<Errors errors={error} />)
 
       expect(wrapper).to.contain(title)
@@ -16,10 +16,9 @@ describe('<Errors />', () => {
   })
 
   describe('Several errors', () => {
-
-    it('Should display a sentence in the plural', () => {
+    it('should display a sentence in the plural', () => {
       const errors = ['une erreur', 'une deuxième erreur']
-      const title = <h3>Des erreurs sont survenues :</h3>
+      const title = <h3>Des erreurs sont survenues</h3>
       const wrapper = shallow(<Errors errors={errors} />)
 
       expect(wrapper).to.contain(title)
@@ -29,8 +28,8 @@ describe('<Errors />', () => {
   describe('When at least one error exists', () => {
     it('Should display all errors', () => {
       const errors = ['une erreur', 'une deuxième erreur']
-      const error1 = <div>une erreur</div>
-      const error2 = <div>une deuxième erreur</div>
+      const error1 = <li>une erreur</li>
+      const error2 = <li>une deuxième erreur</li>
       const wrapper = shallow(<Errors errors={errors} />)
 
       expect(wrapper).to.contain(error1)
