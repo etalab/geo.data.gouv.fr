534e5e045915379c187c73980dcb3f35f87b93eb
tmerlier <tmerlier@student.42.fr>
2017-02-10 14:05:59 +0100

    Add react-copy-to-clipboard

--- src/modules/Datasets/components/DatasetSection/DatasetSection.js
+++ src/modules/Datasets/components/DatasetSection/DatasetSection.js
@@ -1,39 +1,53 @@
-import React from 'react'
+import React, { Component } from 'react'
 
+import CopyToClipboard from 'react-copy-to-clipboard'
 import MarkdownViewer from '../Markdown/MarkdownViewer'
 
 import { doneSince } from '../../../../helpers/doneSince'
 
-import { section, container, resume, infos } from './DatasetSection.css'
+import { section, container, copy, resume, infos } from './DatasetSection.css'
 
-const DatasetSection = ({dataset, style}) => {
-  const type = dataset.metadata.type || 'inconnu'
-  const license = dataset.metadata.license || 'non déterminé'
-  const revisionDate = doneSince(dataset.revisionDate)
+class DatasetSection extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { copied: false }
+  }
 
-  return (
-    <div className={container}>
-      <div className={resume}>
-        <h1>{dataset.metadata.title}</h1>
-        <div className={infos}>
-          <div>Type : <span>{type}</span></div>
-          <div>Licence : <span>{license}</span></div>
-          <div>Dernière mise à jour : <span>{revisionDate}</span></div>
+  render() {
+    const { dataset } = this.props
+    const type = dataset.metadata.type || 'inconnu'
+    const license = dataset.metadata.license || 'non déterminé'
+    const revisionDate = doneSince(dataset.revisionDate)
+
+    return (
+      <div className={container}>
+        <div className={resume}>
+          <h1>{dataset.metadata.title}</h1>
+          <div className={infos}>
+            <div>Type : <span>{type}</span></div>
+            <div>Licence : <span>{license}</span></div>
+            <div>Dernière mise à jour : <span>{revisionDate}</span></div>
+          </div>
+          <div>Identifiant du jeu de données :
+            <CopyToClipboard text={dataset.metadata.id} onCopy={() => this.setState({copied: true})}>
+              <span className={copy}>{dataset.metadata.id}{this.state.copied ? <div>copié !</div> : null}</span>
+            </CopyToClipboard>
+          </div>
         </div>
-        <div>Identifiant du jeu de données : <span>{dataset.metadata.id}</span></div>
-      </div>
 
-      <div className={section}>
-        {!dataset.metadata.description || !dataset.metadata.description.length ?
-          <div>Aucune description.</div> :
-          <MarkdownViewer markdown={dataset.metadata.description} />
-        }
-        <p>
-          <b>Origine de la donnée : </b>{dataset.metadata.lineage ? dataset.metadata.lineage : 'Non renseignée'}
-        </p>
+
+        <div className={section}>
+          {!dataset.metadata.description || !dataset.metadata.description.length ?
+            <div>Aucune description.</div> :
+            <MarkdownViewer markdown={dataset.metadata.description} />
+          }
+          <p>
+            <b>Origine de la donnée : </b>{dataset.metadata.lineage ? dataset.metadata.lineage : 'Non renseignée'}
+          </p>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default DatasetSection
