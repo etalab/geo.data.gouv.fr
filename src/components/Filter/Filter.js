6362ee5995238753c6cc5f4a73a359287e78414d
tmerlier <tmerlier@student.42.fr>
2017-02-16 10:49:27 +0100

    Translate filters

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,13 +1,16 @@
 import React from 'react'
 
+import { filterTradTable } from '../../helpers/manageFilters'
+
 import { link } from './Filter.css'
 
 const Filter = (props) => {
   const { detail, remove, filter, style, onClick } = props
+  const title = filterTradTable[filter.name]
 
   return (
-    <button className={link} title={`${filter.name}: ${filter.value}`} style={style} onClick={() => onClick && onClick(filter)}>
-      { detail && `${filter.name}: ` }{filter.value}
+    <button className={link} title={`${title}: ${filter.value}`} style={style} onClick={() => onClick && onClick(filter)}>
+      { detail && `${title}: ` }{filter.value}
       { remove && <span>&nbsp;<i className="fa fa-close" /></span>}
     </button>
   )
