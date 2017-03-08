fc2d6e858946ff025bdb2953214274c92c9abf5a
tmerlier <tmerlier@student.42.fr>
2017-03-08 23:08:14 +0100

    Format date

--- src/modules/Datasets/components/TechnicalInformations/TechnicalInformations.js
+++ src/modules/Datasets/components/TechnicalInformations/TechnicalInformations.js
@@ -1,19 +1,23 @@
 import React from 'react'
+import moment from 'moment'
+
 import { doneSince } from '../../../../helpers/doneSince'
+import { frequencies } from '../../../../helpers/frequencies'
 
 import { container, histo } from './TechnicalInformations.css'
 
-const TechnicalInformations = ({ dataset, technicalInformations }) => {
-  const { type, license, id } = dataset.metadata
-  const revisionDate = doneSince(dataset.revisionDate)
+const TechnicalInformations = ({ dataset }) => {
+  const { createdAt, updatedAt } = dataset
+  const { type, license, updateFrequency, id } = dataset.metadata
+  const revisionDate = doneSince(updatedAt)
   const other = {} // Waiting for data
 
   return (
     <div className={container}>
       <div className={histo}>
         <h4>Historique de la donnée</h4>
-        {history.updateFrequency ? <div>Fréquence de mise à jour : <b>{history.updateFrequency}</b></div> : null}
-        <div>Date de création : <b>{history.creationDate}</b></div>
+        {updateFrequency ? <div>Fréquence de mise à jour : <b>{frequencies[updateFrequency]}</b></div> : null}
+        <div>Date de création : <b>{moment(createdAt).format('DD/MM/YYYY')}</b></div>
         <div>Dernière mise à jour : <b>{revisionDate}</b></div>
       </div>
       <div>
