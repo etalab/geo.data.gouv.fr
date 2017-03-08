4d2a3087acbbbfb178eb4cc4f2cb30aeafb08f39
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2017-03-08 22:48:41 +0100

    Fix merge issue

--- src/modules/Datasets/components/Producer/Producer.js
+++ src/modules/Datasets/components/Producer/Producer.js
@@ -31,7 +31,7 @@ class Producer extends Component {
     return (
       <div className={container}>
         <img src={dataset ? dataset.organization.logo : '/assets/avatar.png'} alt="producer logo" />
-        <h4>{producerName}</h4>
+        <h4>{dataset.organization.name}</h4>
       </div>
     )
   }
