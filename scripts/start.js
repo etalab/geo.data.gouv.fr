c7431104c4edfefec9ba1e921ddbf1950c9e161c
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2017-03-29 13:50:54 +0200

    Drop dotenv

--- scripts/start.js
+++ scripts/start.js
@@ -1,12 +1,6 @@
 /* eslint no-console: off */
 process.env.NODE_ENV = 'development';
 
-// Load environment variables from .env file. Surpress warnings using silent
-// if this file is missing. dotenv will never modify any environment variables
-// that have already been set.
-// https://github.com/motdotla/dotenv
-require('dotenv').config({silent: true});
-
 var chalk = require('chalk');
 var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
--- scripts/test.js
+++ scripts/test.js
@@ -1,11 +1,6 @@
 process.env.NODE_ENV = 'test';
 process.env.PUBLIC_URL = '';
 
-// Load environment variables from .env file. Surpress warnings using silent
-// if this file is missing. dotenv will never modify any environment variables
-// that have already been set.
-// https://github.com/motdotla/dotenv
-require('dotenv').config({silent: true});
 require('babel-register')();
 
 var jsdom = require('jsdom').jsdom;
