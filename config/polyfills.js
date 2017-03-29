cdb6684ab8196223465a6f86e9d45522a8489a74
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2017-03-29 14:17:26 +0200

    Remove duplicate Object.assign polyfill

--- config/polyfills.js
+++ config/polyfills.js
@@ -1,6 +1,2 @@
 // fetch() polyfill for making API calls.
 require('whatwg-fetch');
-
-// Object.assign() is commonly used with React.
-// It will use the native implementation if it's present and isn't buggy.
-Object.assign = require('object-assign');
