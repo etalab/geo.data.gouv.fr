e8a5ee49300b64ff9fc386269cc81b790d2de68a
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2017-03-29 14:19:23 +0200

    Use fetch polyfill from polyfill.io

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -42,8 +42,6 @@ module.exports = {
     // require.resolve('webpack-dev-server/client') + '?/',
     // require.resolve('webpack/hot/dev-server'),
     require.resolve('react-dev-utils/webpackHotDevClient'),
-    // We ship a few polyfills by default:
-    require.resolve('./polyfills'),
     // Finally, this is your app's code:
     paths.appIndexJs
     // We include the app code last so that if there is a runtime error during
