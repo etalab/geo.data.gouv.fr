bd0e63c743a168e44b715df3acf386578456d0ac
mariondr <desboeufs.m@gmail.com>
2017-04-21 14:46:02 +0200

    Suppression atelier #5 et ajout atelier #7 (#385)
    * Suppression atelier #5 et ajout atelier #7
    
    sur inspire.data.gouv.fr/events
    
    * Ajout atelier #5 dans les anciens

--- src/modules/Events/pages/Events/Events.js
+++ src/modules/Events/pages/Events/Events.js
@@ -11,6 +11,7 @@ const pastEvents = [
   {name: 'Atelier #1', date: '06/10/2016', link: '/assets/ateliers/synthese_atelier_1.pdf'},
   {name: 'Atelier #3', date: '09/02/2017', link: '/assets/ateliers/synthese_atelier_3.pdf'},
   {name: 'Atelier #4', date: '09/03/2017', link: '/assets/ateliers/synthese_atelier_4.pdf'},
+  {name: 'Atelier #5', date: '18/04/2017', link: ''},
 ]
 
 const Events = () => {
@@ -21,8 +22,8 @@ const Events = () => {
 
         <h1>Événements à venir</h1>
         <div className={eventsList}>
-          <EventbriteWidget src="https://www.eventbrite.fr/countdown-widget?eid=32256259340"/>
           <EventbriteWidget src="https://www.eventbrite.fr/countdown-widget?eid=32842679338"/>
+          <EventbriteWidget src="https://www.eventbrite.fr/countdown-widget?eid=33885350997"/>
         </div>
 
         <h1>Événements passés</h1>
