0370e90c91a59d9f91190922c67f5ab8d089961d
mariondr <desboeufs.m@gmail.com>
2017-04-21 10:55:57 +0200

    Ajout atelier #5 dans les anciens

--- src/modules/Events/pages/Events/Events.js
+++ src/modules/Events/pages/Events/Events.js
@@ -11,6 +11,7 @@ const pastEvents = [
   {name: 'Atelier #1', date: '06/10/2016', link: '/assets/ateliers/synthese_atelier_1.pdf'},
   {name: 'Atelier #3', date: '09/02/2017', link: '/assets/ateliers/synthese_atelier_3.pdf'},
   {name: 'Atelier #4', date: '09/03/2017', link: '/assets/ateliers/synthese_atelier_4.pdf'},
+  {name: 'Atelier #5', date: '18/04/2017', link: ''},
 ]
 
 const Events = () => {
