8b184bdd0b973a03118c7f3cd118328ab906fb2c
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2017-07-21 14:04:16 +0200

    Fix listening scope

--- build/scripts/start.js
+++ build/scripts/start.js
@@ -2,6 +2,6 @@ const { development } = require('../../project.config')
 const logger = require('../lib/logger')
 
 logger.info('Starting server…')
-require('../../server').listen(development.port, development.host, () => {
-  logger.success(`Server is running at http://${development.host}:${development.port}`)
+require('../../server').listen(development.port, () => {
+  logger.success(`Server is listening on port ${development.port}`)
 })
