a4c381b75ddeac55303c8d13e202e2bed81159a7
Bertrand Marron <bertrand.marron@gmail.com>
2017-07-27 21:07:44 +0200

    Rewrite Button component
    Stop shadowing the event.
    Allow setting the type and default it to `button` as it defaults to
    `submit`.
    Export all buttons in common/components/Buttons.

--- src/routes/Dataset/components/DatasetDataGouvPublication/DatasetDataGouvPublication.js
+++ src/routes/Dataset/components/DatasetDataGouvPublication/DatasetDataGouvPublication.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import Button from 'common/components/Buttons/Button'
+import { Button } from 'common/components/Buttons'
 
 import { checkLicense, checkProducers, checkDataAvailability } from 'common/helpers/dataGouvChecks'
 
