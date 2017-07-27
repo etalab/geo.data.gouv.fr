14a79b4f67ec28707ffe6369d81f4b5321b80310
Bertrand Marron <bertrand.marron@gmail.com>
2017-07-27 15:35:30 +0200

    Update CenteredMap component

--- src/components/CenteredMap.js
+++ src/components/CenteredMap.js
@@ -1,35 +1,39 @@
-import React, { Component } from 'react'
+import React from 'react'
+import PropTypes from 'prop-types'
 import { Map, TileLayer, GeoJSON } from 'react-leaflet'
 
-const MAP = {
-  lat: 47,
-  lon: 1,
-  zoom: 4,
-  osmUrl: 'https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png',
-  osmAttribution: '&copy; Contributeurs <a href="http://osm.org/copyright">OpenStreetMap</a>',
-}
 
-class CenteredMap extends Component {
+class CenteredMap extends React.PureComponent {
+  static propTypes = {
+    vectors: PropTypes.object.isRequired,
+    className: PropTypes.string,
+    frozen: PropTypes.bool,
+
+    lat: PropTypes.number,
+    lon: PropTypes.number,
+    zoom: PropTypes.number
+  }
+
+  static defaultProps = {
+    frozen: false,
+    lat: 47,
+    lon: 1,
+    zoom: 4
+  }
 
   componentDidMount() {
-    const { vectors, map } = this.refs
-
-    if (vectors && map) {
-      const bounds = vectors.leafletElement.getBounds()
-      map.leafletElement.fitBounds(bounds)
+    if (this.vectors && this.map) {
+      const bounds = this.vectors.leafletElement.getBounds()
+      this.map.leafletElement.fitBounds(bounds)
     }
   }
 
   render() {
-    const zoom = this.props.zoom || MAP.zoom
-    const lat = this.props.lat || MAP.lat
-    const lon = this.props.lon || MAP.lon
-
-    const { vectors, className, frozen } = this.props
+    const { vectors, className, frozen, lat, lon, zoom } = this.props
 
     return (
       <Map
-        ref="map"
+        ref={map => { this.map = map }}
         className={className}
         center={[lat, lon]}
         zoom={zoom}
@@ -38,8 +42,18 @@ class CenteredMap extends Component {
         doubleClickZoom={false}
         zoomControl={!frozen}
       >
-        <TileLayer attribution={MAP.osmAttribution} url={MAP.osmUrl} />
-        <GeoJSON color='blue' fillOpacity={0.1} weight={2} ref="vectors" data={vectors} />
+        <TileLayer
+          attribution='© Contributeurs <a href="http://osm.org/copyright">OpenStreetMap</a>'
+          url='https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png'
+        />
+
+        <GeoJSON
+          color='blue'
+          fillOpacity={0.1}
+          weight={2}
+          ref={vectors => { this.vectors = vectors }}
+          data={vectors}
+        />
       </Map>
     )
   }
