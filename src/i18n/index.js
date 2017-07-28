184aba743be26d77a91fb1a48aeb04dc563869a7
Bertrand Marron <bertrand.marron@gmail.com>
2017-07-28 12:13:40 +0200

    Set moment default locale in i18next init

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,32 +1,40 @@
-import i18n from 'i18next'
+import i18next from 'i18next'
 import LanguageDetector from 'i18next-browser-languagedetector'
+import moment from 'moment'
 
-export default () => i18n
-  .use(LanguageDetector)
-  .init({
-    resources: {
-      en: {
-        Common: require('../locales/en.json')
+
+export default () => {
+  const i18n = i18next
+    .use(LanguageDetector)
+    .init({
+      resources: {
+        en: {
+          Common: require('../locales/en.json')
+        },
+        fr: {
+          Common: require('../locales/fr.json')
+        }
       },
-      fr: {
-        Common: require('../locales/fr.json')
+
+      whitelist: [
+        'en',
+        'fr'
+      ],
+
+      fallbackLng: 'en',
+
+      defaultNS: 'Common',
+
+      interpolation: {
+        formatSeparator: ','
+      },
+
+      react: {
+        wait: true
       }
-    },
+    })
 
-    whitelist: [
-      'en',
-      'fr'
-    ],
+    moment.locale(i18n.language)
 
-    fallbackLng: 'en',
-
-    defaultNS: 'Common',
-
-    interpolation: {
-      formatSeparator: ','
-    },
-
-    react: {
-      wait: true
-    }
-  })
+    return i18n
+}
