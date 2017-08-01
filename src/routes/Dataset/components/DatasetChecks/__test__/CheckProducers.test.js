d567cc2e60bd265e6ccb045f1dc4eeb7c4288c1a
tmerlier <tmerlier@student.42.fr>
2017-08-01 17:20:03 +0200

    CheckProducers trad

--- src/routes/Dataset/components/DatasetChecks/__test__/CheckProducers.test.js
+++ src/routes/Dataset/components/DatasetChecks/__test__/CheckProducers.test.js
@@ -1,27 +1,29 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 
-import CheckProducers from '../CheckProducers'
+import { CheckProducers } from '../CheckProducers'
+
+const translateMock = k => k
 
 describe('<CheckProducers />', () => {
   it('should return true when the array contains at least one element', () => {
     const organizations = ['producteur']
-    const wrapper = shallow(<CheckProducers organizations={organizations} valid />)
+    const wrapper = shallow(<CheckProducers organizations={organizations} valid t={translateMock} />)
 
-    expect(wrapper.html()).to.contain('Au moins un producteur est identifié.')
+    expect(wrapper.html()).to.contain('identifiedProducer')
   })
 
   it('should return false when the array contains no elements', () => {
     const organizations = []
-    const wrapper = shallow(<CheckProducers organizations={organizations} valid={false} />)
+    const wrapper = shallow(<CheckProducers organizations={organizations} valid={false} t={translateMock} />)
 
-    expect(wrapper.html()).to.contain('Le producteur n’a pas été identifié.')
+    expect(wrapper.html()).to.contain('unindentifiedProducer')
   })
 
   it('should return false when the array is undefined', () => {
     const organizations = undefined
-    const wrapper = shallow(<CheckProducers organizations={organizations} valid={false} />)
+    const wrapper = shallow(<CheckProducers organizations={organizations} valid={false} t={translateMock} />)
 
-    expect(wrapper.html()).to.contain('Le producteur n’a pas été identifié.')
+    expect(wrapper.html()).to.contain('unindentifiedProducer')
   })
 })
