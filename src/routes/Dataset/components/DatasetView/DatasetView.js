0de8a00ebcfe7b1e1e2f5e351ec24a22bc042ce3
Bertrand Marron <bertrand.marron@gmail.com>
2017-08-01 02:46:00 +0200

    Add missing dataset components

--- src/routes/Dataset/components/DatasetView/DatasetView.js
+++ src/routes/Dataset/components/DatasetView/DatasetView.js
@@ -5,11 +5,14 @@ import DocumentTitle from 'react-document-title'
 import Warning from 'common/components/Warning'
 import Loader from 'common/components/Loader'
 
+import Discussions from '../../../../modules/Datasets/components/Discussions/Discussions'
+
 import DatasetBlock from '../DatasetBlock'
 import DatasetHeader from '../DatasetHeader'
 import DatasetTechnicalInfo from '../DatasetTechnicalInfo'
 import DatasetDownloadList from '../DatasetDownloadList'
 import DatasetLinks from '../DatasetLinks'
+import DatasetFilters from '../DatasetFilters'
 import DatasetProducer from '../DatasetProducer'
 import DatasetThumbnails from '../DatasetThumbnails'
 import DatasetSpatialExtent from '../DatasetSpatialExtent'
@@ -77,68 +80,85 @@ class DatasetView extends React.PureComponent {
             </Warning>
           )}
           <div className={styles.container}>
-            <div className={styles.main}>
-              <DatasetHeader dataset={dataset} />
+            <div className={styles.inner}>
+              <div className={styles.main}>
+                <DatasetHeader dataset={dataset} />
 
-              <DatasetBlock title='Informations techniques'>
-                <DatasetTechnicalInfo dataset={dataset} status={status} />
-              </DatasetBlock>
+                <DatasetBlock title='Informations techniques'>
+                  <DatasetTechnicalInfo dataset={dataset} status={status} />
+                </DatasetBlock>
 
-              <DatasetBlock title='Téléchargements'>
-                {dataset.dataset.distributions.length > 0 ? (
-                  <DatasetDownloadList distributions={dataset.dataset.distributions} fetchGeoJson={fetchGeoJson} />
-                ) : (
-                  <div>Aucune donnée n'est téléchargeable.</div>
+                <DatasetBlock title='Téléchargements'>
+                  {dataset.dataset.distributions.length > 0 ? (
+                    <DatasetDownloadList distributions={dataset.dataset.distributions} fetchGeoJson={fetchGeoJson} />
+                  ) : (
+                    <div>Aucune donnée n'est téléchargeable.</div>
+                  )}
+                </DatasetBlock>
+
+                <DatasetBlock title='Liens'>
+                  {dataset.metadata.links.length > 0 ? (
+                    <DatasetLinks links={dataset.metadata.links} />
+                  ) : (
+                    <div>Aucun lien disponible.</div>
+                  )}
+                </DatasetBlock>
+
+                <DatasetBlock title='Discussions'>
+                  <Discussions remoteId={publication.remoteId}/>
+                </DatasetBlock>
+
+                <DatasetBlock title='Filtres'>
+                  <DatasetFilters
+                    keywords={dataset.metadata.keywords}
+                    organizations={dataset.organizations}
+                  />
+                </DatasetBlock>
+              </div>
+
+              <div className={styles.aside}>
+                {publication && publication.remoteId && (
+                  <DatasetBlock title='Producteur'>
+                    <Loader loading={dataGouvDataset.pending}>
+                      {dataGouvDataset.dataset && (
+                        <DatasetProducer organization={dataGouvDataset.dataset.organization} />
+                      )}
+                    </Loader>
+                  </DatasetBlock>
                 )}
-              </DatasetBlock>
 
-              <DatasetBlock title='Liens'>
-                {dataset.metadata.links.length > 0 ? (
-                  <DatasetLinks links={dataset.metadata.links} />
-                ) : (
-                  <div>Aucun lien disponible.</div>
+                {dataset.metadata.thumbnails && dataset.metadata.thumbnails.length > 0 && (
+                  <DatasetBlock title='Aperçu des données'>
+                    <DatasetThumbnails recordId={dataset.recordId} thumbnails={dataset.metadata.thumbnails} />
+                  </DatasetBlock>
                 )}
-              </DatasetBlock>
+
+                {dataset.metadata.spatialExtent && (
+                  <DatasetBlock title='Étendue spatiale'>
+                    <DatasetSpatialExtent extent={dataset.metadata.spatialExtent} />
+                  </DatasetBlock>
+                )}
+
+                <DatasetBlock title='Publication sur data.gouv.fr'>
+                  <DatasetDataGouvPublication dataset={dataset} publication={publication} />
+                </DatasetBlock>
+
+                <DatasetBlock title='Contacts'>
+                  <DatasetContactList contacts={dataset.metadata.contacts} />
+                </DatasetBlock>
+
+                {dataset.metadata.credit && (
+                  <DatasetBlock title='Contributions'>
+                    <div>
+                      {dataset.metadata.credit}
+                    </div>
+                  </DatasetBlock>
+                )}
+              </div>
             </div>
 
-            <div className={styles.aside}>
-              {publication && publication.remoteId && (
-                <DatasetBlock title='Producteur'>
-                  <Loader loading={dataGouvDataset.pending}>
-                    {dataGouvDataset.dataset && (
-                      <DatasetProducer organization={dataGouvDataset.dataset.organization} />
-                    )}
-                  </Loader>
-                </DatasetBlock>
-              )}
-
-              {dataset.metadata.thumbnails && dataset.metadata.thumbnails.length > 0 && (
-                <DatasetBlock title='Aperçu des données'>
-                  <DatasetThumbnails recordId={dataset.recordId} thumbnails={dataset.metadata.thumbnails} />
-                </DatasetBlock>
-              )}
-
-              {dataset.metadata.spatialExtent && (
-                <DatasetBlock title='Étendue spatiale'>
-                  <DatasetSpatialExtent extent={dataset.metadata.spatialExtent} />
-                </DatasetBlock>
-              )}
-
-              <DatasetBlock title='Publication sur data.gouv.fr'>
-                <DatasetDataGouvPublication dataset={dataset} publication={publication} />
-              </DatasetBlock>
-
-              <DatasetBlock title='Contacts'>
-                <DatasetContactList contacts={dataset.metadata.contacts} />
-              </DatasetBlock>
-
-              {dataset.metadata.credit && (
-                <DatasetBlock title='Contributions'>
-                  <div>
-                    {dataset.metadata.credit}
-                  </div>
-                </DatasetBlock>
-              )}
+            <div className={styles.footer}>
+              <div>Identifiant du jeu de données : <b>{dataset.metadata.id}</b></div>
             </div>
           </div>
         </div>
