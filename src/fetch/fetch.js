a9a340d042746b5506421f9d816d8c0e8db5a148
Bertrand Marron <bertrand.marron@gmail.com>
2017-08-18 14:59:38 +0200

    Use environment variables for hardcoded urls

--- src/fetch/fetch.js
+++ src/fetch/fetch.js
@@ -1,77 +1,79 @@
 import { _get, _put, _post, _delete } from '../helpers/super'
 
+const { PUBLICATION_API_URL, INSPIRE_API_URL, PROXY_API_URL } = process.env
+
 export function fetchCatalog(catalogId) {
   if (!catalogId) return Promise.reject(new Error('catalogId is required'))
-  return _get(`https://inspire.data.gouv.fr/api/geogw/catalogs/${catalogId}`)
+  return _get(`${INSPIRE_API_URL}/catalogs/${catalogId}`)
 }
 
 export function fetchCatalogs() {
-  return _get('https://inspire.data.gouv.fr/api/geogw/catalogs')
+  return _get(`${INSPIRE_API_URL}/catalogs`)
 }
 
 export function getUser() {
-  return _get('https://inspire.data.gouv.fr/dgv/api/me')
+  return _get(`${PUBLICATION_API_URL}/me`)
 }
 
 export function getOrganization(organizationId) {
   if (!organizationId) return Promise.reject(new Error('organizationId is required'))
-  return _get(`https://inspire.data.gouv.fr/dgv/api/organizations/${organizationId}`)
+  return _get(`${PUBLICATION_API_URL}/organizations/${organizationId}`)
 }
 
 export function getOrganizationDetail(organizationId) {
   if (!organizationId) return Promise.reject(new Error('organizationId is required'))
-  return _get(`https://inspire.data.gouv.fr/dgv/api/organizations/${organizationId}/profile`)
+  return _get(`${PUBLICATION_API_URL}/organizations/${organizationId}/profile`)
 }
 
 export function fetchOrganizationMetrics(organizationId) {
   if (!organizationId) return Promise.reject(new Error('organizationId is required'))
-  return _get(`https://inspire.data.gouv.fr/dgv/api/organizations/${organizationId}/datasets/metrics`)
+  return _get(`${PUBLICATION_API_URL}/organizations/${organizationId}/datasets/metrics`)
 }
 
 export function fetchOrganizationPublished(organizationId) {
   if (!organizationId) return Promise.reject(new Error('organizationId is required'))
-  return _get(`https://inspire.data.gouv.fr/dgv/api/organizations/${organizationId}/datasets/published`)
+  return _get(`${PUBLICATION_API_URL}/organizations/${organizationId}/datasets/published`)
 }
 
 export function fetchOrganizationNotPublishedYet(organizationId) {
   if (!organizationId) return Promise.reject(new Error('organizationId is required'))
-  return _get(`https://inspire.data.gouv.fr/dgv/api/organizations/${organizationId}/datasets/not-published-yet`)
+  return _get(`${PUBLICATION_API_URL}/organizations/${organizationId}/datasets/not-published-yet`)
 }
 
 export function fetchOrganizationPublishedByOthers(organizationId) {
   if (!organizationId) return Promise.reject(new Error('organizationId is required'))
-  return _get(`https://inspire.data.gouv.fr/dgv/api/organizations/${organizationId}/datasets/published-by-others`)
+  return _get(`${PUBLICATION_API_URL}/organizations/${organizationId}/datasets/published-by-others`)
 }
 
 export function publishDataset(datasetId, organizationId) {
   if (!datasetId) return Promise.reject(new Error('datasetId is required'))
   if (!organizationId) return Promise.reject(new Error('organizationId is required'))
-  const url = `https://inspire.data.gouv.fr/dgv/api/datasets/${datasetId}/publication`
+  const url = `${PUBLICATION_API_URL}/datasets/${datasetId}/publication`
 
   return _put(url, { organization: organizationId })
 }
 
 export function updateOrganizationAccount(organizationId, organization = {}) {
   if (!organizationId) return Promise.reject(new Error('organizationId is required'))
-  const url = `https://inspire.data.gouv.fr/dgv/api/organizations/${organizationId}`
+  const url = `${PUBLICATION_API_URL}/organizations/${organizationId}`
   return _put(url, organization)
 }
 
 export function getOrganizationProducers(organizationId) {
   if (!organizationId) return Promise.reject(new Error('organizationId is required'))
-  return _get(`https://inspire.data.gouv.fr/dgv/api/organizations/${organizationId}/producers`)
+  return _get(`${PUBLICATION_API_URL}/organizations/${organizationId}/producers`)
 }
 
 export function dissociateProducer(producerId, organizationId) {
   if (!producerId && organizationId) return Promise.reject(new Error('producerId and organizationId is required'))
-  const url = `https://inspire.data.gouv.fr/dgv/api/organizations/${organizationId}/producers/${producerId}`
+  const url = `${PUBLICATION_API_URL}/organizations/${organizationId}/producers/${producerId}`
 
   return _delete(url)
 }
 
 export function associateProducer(producerId, organizationId) {
   if (!producerId && organizationId) return Promise.reject(new Error('producerId and organizationId is required'))
-  const url = `https://inspire.data.gouv.fr/dgv/api/organizations/${organizationId}/producers`
+  const url = `${PUBLICATION_API_URL}/organizations/${organizationId}/producers`
   const params = { '_id': producerId }
 
   return _post(url, params)
@@ -79,7 +81,7 @@ export function associateProducer(producerId, organizationId) {
 
 export function getProducersToAssociate(catalogId) {
   if (!catalogId) return Promise.reject(new Error('catalogId is required'))
-  const url = `https://inspire.data.gouv.fr/api/geogw/services/${catalogId}/records?resultParts=facets&opendata=yes&availability=yes&facets[keyword]=0`
+  const url = `${INSPIRE_API_URL}/services/${catalogId}/records?resultParts=facets&opendata=yes&availability=yes&facets[keyword]=0`
 
   return _get(url)
 }
@@ -91,19 +93,19 @@ export function getOrganizations(organizationsId) {
 // DATA.GOUV.FR
 export function getDiscussions(datasetId) {
   if (!datasetId) return Promise.reject(new Error('datasetId is required'))
-  const url = `https://inspire.data.gouv.fr/dgv/proxy-api/1/discussions/?for=${datasetId}`
+  const url = `${PROXY_API_URL}/discussions/?for=${datasetId}`
 
   return _get(url)
 }
 
 export function createNewDiscussion(discussion) {
-  const url = 'https://inspire.data.gouv.fr/dgv/proxy-api/1/discussions/'
+  const url = `${PROXY_API_URL}/discussions/`
 
   return _post(url, discussion)
 }
 
 export function createNewReply(content, discussionId) {
-  const url = 'https://inspire.data.gouv.fr/dgv/proxy-api/1/discussions/' + discussionId + '/'
+  const url = `${PROXY_API_URL}/discussions/${discussionId}/`
 
   return _post(url, content)
 }
