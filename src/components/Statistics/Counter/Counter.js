4a5975c911301b8f68b11464836a7ff427e4ced8
tmerlier <tmerlier@student.42.fr>
2017-08-25 15:10:53 +0200

    Remove classnames dependencie

--- src/components/Statistics/Counter/Counter.js
+++ src/components/Statistics/Counter/Counter.js
@@ -1,36 +1,17 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import cx from 'classnames'
 
 import styles from './Counter.scss'
 
-const Counter = ({ value, label, unit = '', size = '', color = '', icon = '', title = '' }) => {
-  const labelSize = cx(styles.medium, {
-    [styles.large]: size === 'large',
-    [styles.small]: size === 'small'
-  })
-
-  const labelColor = cx(styles.medium, {
-    [styles.success]: color === 'success',
-    [styles.warning]: color === 'warning',
-    [styles.error]: color === 'error'
-  })
-
-  const labelStyle = size === 'small' ? styles.small : styles.defaultLabel
-
-  const titleDiv = title ? <h3>{title}</h3> : null
-  const iconDiv = icon ? <i className={`${icon} icon`} /> : null
-  const valueDiv = <div className={`${labelColor} ${labelSize}`}>{iconDiv} {value || '0'} {unit}</div>
-  const labelDiv = label ? <div className={labelStyle}>{label}</div> : null
-
-  return (
-    <div className={styles.container}>
-      { titleDiv }
-      { valueDiv }
-      { labelDiv }
+const Counter = ({ value, label, unit = '', size = '', color = '', icon = '', title = '' }) => (
+  <div className={styles.container}>
+    { title && <h3>{title}</h3> }
+    <div className={`${styles[color]} ${styles[size]}`}>
+      {icon && <i className={`${icon} icon`} />} {value || '0'} <div className={styles.unit}>{unit}</div>
     </div>
-  )
-}
+    { label && <div className={`${styles[size]}`}>{label}</div> }
+  </div>
+)
 
 Counter.propTypes = {
   value: PropTypes.number.isRequired,
