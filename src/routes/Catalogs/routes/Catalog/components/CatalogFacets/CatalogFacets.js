cbdc059b6fd15a3fa988974d78cd53174ce19052
Bertrand Marron <bertrand.marron@gmail.com>
2017-08-28 17:34:04 +0200

    Fix catalog filter names translations

--- src/routes/Catalogs/routes/Catalog/components/CatalogFacets/CatalogFacets.js
+++ src/routes/Catalogs/routes/Catalog/components/CatalogFacets/CatalogFacets.js
@@ -5,14 +5,14 @@ import Facet from 'common/components/Facets/Facet'
 
 import styles from './CatalogFacets.scss'
 
-const CatalogFacets = ({ title, filters, onSearch }) => (
+const CatalogFacets = ({ title, type, filters, onSearch }) => (
   <div className={styles.container}>
     <h2>{title}</h2>
 
     {Object.entries(filters).map(([key, filter]) => (
       <div key={key} className={styles.facet}>
         <Facet
-          name={title}
+          name={type}
           value={key}
           count={filter}
           addFilter={onSearch}
@@ -24,7 +24,7 @@ const CatalogFacets = ({ title, filters, onSearch }) => (
 
 CatalogFacets.propTypes = {
   title: PropTypes.string.isRequired,
-
+  type: PropTypes.string.isRequired,
   filters: PropTypes.object.isRequired,
 
   onSearch: PropTypes.func.isRequired
