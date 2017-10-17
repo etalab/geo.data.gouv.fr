f2d16a162878760ba62d69518ce136dad77ad6a8
Bertrand Marron <bertrand.marron@gmail.com>
2017-10-17 14:38:18 +0200

    Break overflowing words in markdown
    Fix style for mobile.

--- src/routes/Search/components/SearchResult/SearchResult.js
+++ src/routes/Search/components/SearchResult/SearchResult.js
@@ -3,11 +3,11 @@ import { translate } from 'react-i18next'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 
-import Filter from 'common/components/Filter'
+// import Filter from 'common/components/Filter'
 import MarkdownSummary from 'common/components/MarkdownSummary'
 
 import styles from './SearchResult.scss'
-import noThumbnail from './images/no-thumbnail.svg'
+// import noThumbnail from './images/no-thumbnail.svg'
 
 const { GEODATA_API_URL } = process.env
 
@@ -15,56 +15,57 @@ const SearchResult = ({ dataset, addFilter, t }) => (
   <div className={styles.container}>
     <Link to={`/datasets/${dataset.recordId}`} className={styles.dataset}>
       <div className={styles.thumbnail}>
-        {dataset.metadata.thumbnails && dataset.metadata.thumbnails.length
-          ? <img src={`${GEODATA_API_URL}/records/${dataset.recordId}/thumbnails/${dataset.metadata.thumbnails[0].originalUrlHash}`} alt='thumbnail' />
-          : <img src={noThumbnail} alt='no image available' />
-        }
+        {dataset.metadata.thumbnails && dataset.metadata.thumbnails.length > 0 && (
+          <img src={`${GEODATA_API_URL}/records/${dataset.recordId}/thumbnails/${dataset.metadata.thumbnails[0].originalUrlHash}`} alt='thumbnail' />
+        )}
       </div>
 
       <div className={styles.content}>
-        <div className={styles.description}>
-          <h3>{dataset.metadata.title}</h3>
+        <div>
+          <div className={styles.title}>
+            <h3>{dataset.metadata.title}</h3>
 
-          { dataset.metadata.description && (
+            {dataset.metadata.inspireTheme && (
+              <div className={styles.inspire}>
+                <img src={`/assets/inspire-icons/${dataset.metadata.inspireTheme.id}.svg`} alt='Inspire theme' />
+              </div>
+            )}
+          </div>
+
+          {dataset.metadata.description && (
             <MarkdownSummary markdown={dataset.metadata.description} />
           )}
         </div>
 
-        <div className={styles.metadata}>
-          {dataset.metadata.inspireTheme && (
-            <img src={`/assets/inspire-icons/${dataset.metadata.inspireTheme.id}.svg`} alt='Inspire theme' />
-          )}
-        </div>
+        {/* <div className={styles.filters}>
+          <div className={styles.facet}>
+            <h5>{t('SearchResult.keywords', { context: dataset.metadata.keywords.length })}</h5>
+            <div className={styles.list}>
+              {dataset.metadata.keywords.map((keyword, idx) => (
+                <Filter
+                  key={idx}
+                  onClick={addFilter}
+                  filter={{ value: keyword, name: 'keyword' }}
+                />
+              ))}
+            </div>
+          </div>
+
+          <div className={styles.facet}>
+            <h5>{t('SearchResult.organizations', { context: dataset.organizations.length })}</h5>
+            <div className={styles.list}>
+              {dataset.organizations.map((organization, idx) => (
+                <Filter
+                  key={idx}
+                  onClick={addFilter}
+                  filter={{ value: organization, name: 'organization' }}
+                />
+              ))}
+            </div>
+          </div>
+        </div> */}
       </div>
     </Link>
-
-    <div className={styles.filters}>
-      <div className={styles.facet}>
-        <h5>{t('SearchResult.keywords', { context: dataset.metadata.keywords.length })}</h5>
-        <div className={styles.list}>
-          {dataset.metadata.keywords.map((keyword, idx) => (
-            <Filter
-              key={idx}
-              onClick={addFilter}
-              filter={{ value: keyword, name: 'keyword' }}
-            />
-          ))}
-        </div>
-      </div>
-
-      <div className={styles.facet}>
-        <h5>{t('SearchResult.organizations', { context: dataset.organizations.length })}</h5>
-        <div className={styles.list}>
-          {dataset.organizations.map((organization, idx) => (
-            <Filter
-              key={idx}
-              onClick={addFilter}
-              filter={{ value: organization, name: 'organization' }}
-            />
-          ))}
-        </div>
-      </div>
-    </div>
   </div>
 )
 
