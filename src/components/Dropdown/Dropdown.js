cde6a73e4699dc8aa4064037b2212d6a75169f19
Bertrand Marron <bertrand.marron@gmail.com>
2017-10-18 20:52:46 +0200

    Stop using hover for dropdown

--- src/components/Dropdown/Dropdown.js
+++ src/components/Dropdown/Dropdown.js
@@ -3,18 +3,40 @@ import PropTypes from 'prop-types'
 
 import styles from './Dropdown.scss'
 
-const Dropdown = ({ title, children }) => (
-  <div className={styles.dropdown}>
-    {title}
-    <div className={styles.dropdownContent}>
-      {children}
-    </div>
-  </div>
-)
+class Dropdown extends React.Component {
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    children: PropTypes.node
+  }
 
-Dropdown.propTypes = {
-  title: PropTypes.string.isRequired,
-  children: PropTypes.node
+  state = {
+    visible: false
+  }
+
+  toggleDropDown = e => {
+    e.preventDefault()
+
+    this.setState(state => ({
+      visible: !state.visible
+    }))
+  }
+
+  render() {
+    const { title, children } = this.props
+    const { visible } = this.state
+
+    return (
+      <div className={styles.dropdown} onClick={this.toggleDropDown}>
+        <a>{title}</a>
+
+        {visible && (
+          <div className={styles.content}>
+            {children}
+          </div>
+        )}
+      </div>
+    )
+  }
 }
 
 export default Dropdown
