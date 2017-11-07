cc5548aa556497708b32239addfca494baacbc09
Bertrand Marron <bertrand.marron@gmail.com>
2017-11-07 17:28:21 +0100

    Use localized link for dataset link in homepage

--- components/home/hero.js
+++ components/home/hero.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import Link from 'next/link'
 import { translate } from 'react-i18next'
 
+import Link from '../link'
 import Container from '../container'
 import SearchInput from '../search-input'
 
