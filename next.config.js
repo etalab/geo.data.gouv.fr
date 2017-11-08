dbcec2cbd00580ac81a3c3b9340da7cafb895928
Bertrand Marron <bertrand.marron@gmail.com>
2017-11-08 13:38:10 +0100

    Remove useless locales from moment

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
+const webpack = require('webpack')
 const { join } = require('path')
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
 
 module.exports = {
   webpack: function (config, { dev }) {
+    config.plugins.push(
+      new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, /fr/)
+    )
+
     if (!dev) {
       config.plugins.push(new BundleAnalyzerPlugin({
         analyzerMode: 'static',
