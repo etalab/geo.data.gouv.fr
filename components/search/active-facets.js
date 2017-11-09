28834dcc1ca4a84d986abdddb828f1241b70448c
Bertrand Marron <bertrand.marron@gmail.com>
2017-11-09 01:01:00 +0100

    Allow removing active facets

--- components/search/active-facets.js
+++ components/search/active-facets.js
@@ -1,33 +1,70 @@
+import { format } from 'url'
 import React from 'react'
 import PropTypes from 'prop-types'
 import { translate } from 'react-i18next'
+import { withRouter } from 'next/router'
 
 import Facet from '../facet'
 
-const ActiveFacets = ({ facets, t }) => (
-  <div>
-    {facets.map(facet => (
-      <span className='facet' key={`${facet.name}-${facet.value}`}>
-        <Facet facet={facet} detailed removable />
-      </span>
-    ))}
+class ActiveFacets extends React.Component {
+  static propTypes = {
+    facets: PropTypes.array.isRequired,
 
-    <style jsx>{`
-      div {
-        margin-top: 10px;
-      }
+    router: PropTypes.shape({
+      push: PropTypes.func.isRequired,
+      query: PropTypes.object.isRequired
+    }).isRequired,
+    i18n: PropTypes.shape({
+      language: PropTypes.string.isRequired
+    }).isRequired
+  }
 
-      span {
-        display: inline-block;
-        margin: 0 5px 5px 0;
-      }
-    `}</style>
-  </div>
-)
+  removeFacet = filter => {
+    const { router, i18n } = this.props
 
-ActiveFacets.propTypes = {
-  facets: PropTypes.array.isRequired,
-  t: PropTypes.func.isRequired
+    const query = {
+      ...router.query
+    }
+
+    const match = query[filter.name]
+    if (Array.isArray(match)) {
+      query[filter.name] = match.filter(v => v !== filter.value)
+    } else {
+      delete query[filter.name]
+    }
+
+    const url = format({
+      pathname: '/search',
+      query
+    })
+
+    router.push(url, `/${i18n.language}${url}`)
+  }
+
+  render () {
+    const { facets } = this.props
+
+    return (
+      <div>
+        {facets.map(facet => (
+          <span className='facet' key={`${facet.name}-${facet.value}`}>
+            <Facet facet={facet} detailed onClick={this.removeFacet} />
+          </span>
+        ))}
+
+        <style jsx>{`
+          div {
+            margin-top: 10px;
+          }
+
+          span {
+            display: inline-block;
+            margin: 0 5px 5px 0;
+          }
+        `}</style>
+      </div>
+    )
+  }
 }
 
-export default translate('search')(ActiveFacets)
+export default translate('search')(withRouter(ActiveFacets))
