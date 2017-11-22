9d4d798038a75531741a31e1b5d62be0924c970e
Bertrand Marron <bertrand.marron@gmail.com>
2017-11-22 14:03:42 +0100

    Allow specifying a title to Box component

--- components/box.js
+++ components/box.js
@@ -1,22 +1,39 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const Box = ({ children }) => (
-  <div>
-    {children}
+const Box = ({ children, title }) => (
+  <div className='wrapper'>
+    {title && <h3>{title}</h3>}
+    <div className='inner'>
+      {children}
+    </div>
 
     <style jsx>{`
       @import 'colors';
 
-      div {
+      .wrapper {
         background-color: $white;
         box-shadow: 1px 1px 5px rgba(0, 0, 0, 0.3);
-        padding: 1.6em 2em;
         border-radius: 2px;
         margin-bottom: 20px;
+        overflow: hidden;
+      }
+
+      h3 {
+        background: darken($blue, 7%);
+        color: $white;
+        margin: 0;
+        padding: 0.6em 1em;
+        max-width: 100%;
+        overflow: hidden;
+        text-overflow: ellipsis;
+      }
+
+      .inner {
+        padding: 1.6em 2em;
 
         @media (max-width: 551px) {
-          padding: 0.6em 1em;
+          padding: 1em;
         }
       }
     `}</style>
@@ -24,7 +41,8 @@ const Box = ({ children }) => (
 )
 
 Box.propTypes = {
-  children: PropTypes.node
+  children: PropTypes.node,
+  title: PropTypes.string
 }
 
 export default Box
