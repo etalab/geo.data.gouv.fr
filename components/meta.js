9fd8cb6478a229c7f1339a8a4e0a2155d58d87df
Bertrand Marron <bertrand.marron@gmail.com>
2017-12-03 16:53:33 +0100

    Update meta component to use Fragments

--- components/meta.js
+++ components/meta.js
@@ -1,28 +1,36 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import PropTypes from 'prop-types'
 import Head from 'next/head'
 
 const SITE_NAME = 'geo.data.gouv.fr'
 
-const Meta = ({ title, description, image }) => (
+const Meta = ({ title, description, images }) => (
   <Head>
     <title>{title} | {SITE_NAME}</title>
     <meta name='twitter:title' content={title} />
     <meta property='og:title' content={title} />
 
-    {description && <meta name='description' content={description} />}
-    {description && <meta name='twitter:description' content={description} />}
-    {description && <meta name='og:description' content={description} />}
+    {description && (
+      <Fragment>
+        <meta name='description' content={description} />
+        <meta name='twitter:description' content={description} />
+        <meta name='og:description' content={description} />
+      </Fragment>
+    )}
 
-    {image && <meta name='twitter:image' content={description} />}
-    {image && <meta name='og:image' content={description} />}
+    {images && images.length > 0 && images.map((image, idx) => (
+      <Fragment key={idx}>
+        <meta name='twitter:image' content={image} />
+        <meta name='og:image' content={image} />
+      </Fragment>
+    ))}
   </Head>
 )
 
 Meta.propTypes = {
   title: PropTypes.string.isRequired,
   description: PropTypes.string,
-  image: PropTypes.string
+  images: PropTypes.arrayOf(PropTypes.string)
 }
 
 export default Meta
