8e2f076a10b22a040c1f2ebca8b13216474f5792
Bertrand Marron <bertrand.marron@gmail.com>
2017-12-03 01:55:02 +0100

    Add switcher for table preview

--- components/dataset/downloads/preview.js
+++ components/dataset/downloads/preview.js
@@ -3,9 +3,13 @@ import PropTypes from 'prop-types'
 import dynamic from 'next/dynamic'
 import { translate } from 'react-i18next'
 
+import MapIcon from 'react-icons/lib/fa/map'
+import TableIcon from 'react-icons/lib/fa/table'
+
 import { _get } from '../../../lib/fetch'
 
 import Modal from '../../modal'
+import Button from '../../button'
 
 const CenteredMap = dynamic(import('../../centered-map'), {
   ssr: false,
@@ -22,7 +26,8 @@ class Preview extends React.Component {
 
   state = {
     loading: true,
-    data: null
+    data: null,
+    view: 'map'
   }
 
   async componentDidMount() {
@@ -36,23 +41,49 @@ class Preview extends React.Component {
     }))
   }
 
+  changeView = view => () => {
+    this.setState(() => ({
+      view
+    }))
+  }
+
   render() {
     const { onClose, t } = this.props
-    const { loading, data } = this.state
+    const { loading, data, view } = this.state
 
     return (
       <Modal fluid onClose={onClose}>
         {loading ? t('common:loading') : (
           <div className='preview'>
+            <div className='actions'>
+              <Button
+                size='large'
+                color={view === 'map' ? 'blue' : 'white'}
+                onClick={this.changeView('map')}
+              >
+                <MapIcon />
+              </Button>
+              <Button
+                size='large'
+                color={view === 'table' ? 'blue' : 'white'}
+                onClick={this.changeView('table')}
+              >
+                <TableIcon />
+              </Button>
+            </div>
             <div className='map-wrapper'>
-              <div className='map'>
-                <CenteredMap
-                  vectors={data}
-                  lat={47}
-                  lon={1}
-                  zoom={5.5}
-                />
-              </div>
+              {view === 'map' ? (
+                <div className='map'>
+                  <CenteredMap
+                    vectors={data}
+                    lat={47}
+                    lon={1}
+                    zoom={5.5}
+                  />
+                </div>
+              ) : (
+                'table'
+              )}
             </div>
           </div>
         )}
@@ -73,6 +104,13 @@ class Preview extends React.Component {
             position: absolute;
             height: 100%;
             width: 100%;
+          }
+
+          .actions {
+            :global(button) {
+              margin-right: 7px;
+              margin-bottom: 10px;
+            }
           }
         `}</style>
       </Modal>
