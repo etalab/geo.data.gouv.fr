4fbeaa89f286e426fabfdc4fe15c6d8e3808895b
Bertrand Marron <bertrand.marron@gmail.com>
2017-12-06 12:01:59 +0100

    Add publication organization layout

--- pages/publication/organization.js
+++ pages/publication/organization.js
@@ -2,25 +2,47 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { flowRight } from 'lodash'
 
+import { _get } from '../../lib/fetch'
+
 import withI18n from '../../components/hoc/with-i18n'
 import withAuth from '../../components/hoc/with-auth'
+import withSession from '../../components/hoc/with-session'
 
 import Page from '../../components/page'
 import Meta from '../../components/meta'
 import Content from '../../components/content'
 import Container from '../../components/container'
 import RequireAuth from '../../components/require-auth'
+import Box from '../../components/box'
 
 import Header from '../../components/publication/header'
 
+import { PUBLICATION_BASE_URL } from '@env'
+
 class PublicationPage extends React.Component {
   static propTypes = {
-    organizationId: PropTypes.string.isRequired
+    organizationId: PropTypes.string.isRequired,
+    session: PropTypes.shape({
+      user: PropTypes.object
+    })
   }
 
   static getInitialProps({ query }) {
     return {
       organizationId: query.oid
+    }
+  }
+
+  state = {}
+
+  componentWillReceiveProps(props, state, context) {
+    const { session, organizationId } = props
+
+    if (session && session.user && !this.props.session) {
+      this.setState({
+        organizationPromise: _get(`${PUBLICATION_BASE_URL}/api/organizations/${organizationId}`),
+        metricsPromise: _get(`${PUBLICATION_BASE_URL}/api/organizations/${organizationId}/datasets/metrics`)
+      })
     }
   }
 
@@ -30,7 +52,31 @@ class PublicationPage extends React.Component {
 
     return (
       <div>
+        <Meta title={`${organization.name} | Publication`} />
+
         <Header user={user} organization={organization} />
+        <h3>{organization.name}</h3>
+        <div className='dashboard'>
+          <Box title='Catalogues source' color='blue' />
+          <Box title='Producteurs source' color='blue' />
+          <Box title='Jeux de données' color='blue' />
+        </div>
+
+        <style jsx>{`
+          .dashboard {
+            display: grid;
+            grid-template-columns: repeat(3, 1fr);
+            grid-gap: 4em;
+
+            @media (max-width: 1280px) {
+              grid-gap: 2em;
+            }
+
+            @media (max-width: 768px) {
+              display: block;
+            }
+          }
+        `}</style>
       </div>
     )
   }
@@ -38,8 +84,6 @@ class PublicationPage extends React.Component {
   render() {
     return (
       <Page>
-        <Meta title='Organisation | Publication' />
-
         <Content>
           <Container fluid>
             <RequireAuth
@@ -54,5 +98,6 @@ class PublicationPage extends React.Component {
 
 export default flowRight(
   withI18n(),
-  withAuth()
+  withAuth(),
+  withSession()
 )(PublicationPage)
