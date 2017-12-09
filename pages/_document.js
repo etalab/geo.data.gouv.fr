1b055d5a210598789d246bb1d206cd204239ad9f
Bertrand Marron <bertrand.marron@gmail.com>
2017-12-09 12:39:20 +0100

    Add polyfill.io for IE11 compat

---
+++ pages/_document.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Document, { Head, Main, NextScript } from 'next/document'
+import flush from 'styled-jsx/server'
+
+export default class MyDocument extends Document {
+  static getInitialProps({ renderPage }) {
+    const { html, head, errorHtml, chunks } = renderPage()
+    const styles = flush()
+    return { html, head, errorHtml, chunks, styles }
+  }
+
+  render() {
+    return (
+      <html>
+        <Head />
+        <body>
+          <Main />
+          <script src='https://cdn.polyfill.io/v2/polyfill.min.js?features=default,es7' />
+          <NextScript />
+        </body>
+      </html>
+    )
+  }
+}
