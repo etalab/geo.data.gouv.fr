058c790cf8ffda977a7075181b2fc3ff15845d8d
Bertrand Marron <bertrand.marron@gmail.com>
2017-12-13 14:41:54 +0100

    Add tests for catalogs.findCandidates

--- tests/lib/catalog.test.js
+++ tests/lib/catalog.test.js
@@ -1,43 +1,120 @@
-import { isObsolete } from '../../lib/catalog'
+import { isObsolete, findCandidates } from '../../lib/catalog'
 
-test('isObsolete should return false if catalog is not defined', () => {
-  expect(isObsolete()).toBe(false)
+describe('isObsolete', () => {
+  test('return false if catalog is not defined', () => {
+    expect(isObsolete()).toBe(false)
+  })
+
+  test('return false if mostRecentRevisionDate is not defined', () => {
+    expect(isObsolete({
+      metrics: {}
+    })).toBe(false)
+  })
+
+  test('return false if mostRecentRevisionDate is invalid', () => {
+    expect(isObsolete({
+      metrics: {
+        mostRecentRevisionDate: 'not a valid date'
+      }
+    })).toBe(false)
+  })
+
+  test('return true if mostRecentRevisionDate is too old', () => {
+    expect(isObsolete({
+      metrics: {
+        mostRecentRevisionDate: new Date(2000, 0, 1)
+      }
+    })).toBe(true)
+  })
+
+  test('return false if mostRecentRevisionDate is recent', () => {
+    expect(isObsolete({
+      metrics: {
+        mostRecentRevisionDate: new Date()
+      }
+    })).toBe(false)
+  })
+
+  test('work with strings', () => {
+    expect(isObsolete({
+      metrics: {
+        mostRecentRevisionDate: new Date().toISOString()
+      }
+    })).toBe(false)
+  })
 })
 
-test('isObsolete should return false if mostRecentRevisionDate is not defined', () => {
-  expect(isObsolete({
-    metrics: {}
-  })).toBe(false)
-})
+describe('findCandidates', () => {
+  test('ignore catalogs without open or download data', () => {
+    const catalogs = [{}, {}, {}]
 
-test('isObsolete should return false if mostRecentRevisionDate is invalid', () => {
-  expect(isObsolete({
-    metrics: {
-      mostRecentRevisionDate: 'not a valid date'
-    }
-  })).toBe(false)
-})
+    expect(findCandidates(catalogs).length).toBe(0)
+  })
 
-test('isObsolete should return true if mostRecentRevisionDate is too old', () => {
-  expect(isObsolete({
-    metrics: {
-      mostRecentRevisionDate: new Date(2000, 0, 1)
-    }
-  })).toBe(true)
-})
+  test('excludes catalogs with just open data', () => {
+    const catalogs = [{}, {
+      metrics: {
+        datasets: {
+          partitions: { openness: { yes: 1 } }
+        }
+      }
+    }, {}]
 
-test('isObsolete should return false if mostRecentRevisionDate is recent', () => {
-  expect(isObsolete({
-    metrics: {
-      mostRecentRevisionDate: new Date()
-    }
-  })).toBe(false)
-})
+    expect(findCandidates(catalogs).length).toBe(0)
+  })
 
-test('isObsolete should also work with strings', () => {
-  expect(isObsolete({
-    metrics: {
-      mostRecentRevisionDate: new Date().toISOString()
-    }
-  })).toBe(false)
+  test('excludes catalogs with just download data', () => {
+    const catalogs = [{}, {
+      metrics: {
+        datasets: {
+          partitions: { download: { yes: 1 } }
+        }
+      }
+    }, {}]
+
+    expect(findCandidates(catalogs).length).toBe(0)
+  })
+
+  test('include catalogs with download and open data', () => {
+    const catalogs = [{}, {
+      metrics: {
+        datasets: {
+          partitions: {
+            openness: { yes: 1 },
+            download: { yes: 1 }
+          }
+        }
+      }
+    }, {}]
+
+    expect(findCandidates(catalogs).length).toBe(1)
+  })
+
+  test('exclude blacklisted catalogs', () => {
+    const catalogs = [{
+      id: 1,
+      metrics: {
+        datasets: {
+          partitions: {
+            openness: { yes: 1 },
+            download: { yes: 1 }
+          }
+        }
+      }
+    }, {
+      id: 2,
+      metrics: {
+        datasets: {
+          partitions: {
+            openness: { yes: 1 },
+            download: { yes: 1 }
+          }
+        }
+      }
+    }, {}]
+
+    const candidates = findCandidates(catalogs, [1])
+    expect(candidates.length).toBe(1)
+    expect(candidates[0].id).toBe(2)
+  })
 })
