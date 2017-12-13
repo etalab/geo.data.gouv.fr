29b8a27c922fc62afa97a6913af96222ada89ebd
Bertrand Marron <bertrand.marron@gmail.com>
2017-12-13 20:00:13 +0100

    Add tests for catalog.computeScore

--- tests/lib/catalog.test.js
+++ tests/lib/catalog.test.js
@@ -1,4 +1,6 @@
-import { isObsolete, findCandidates } from '../../lib/catalog'
+import moment from 'moment'
+
+import { isObsolete, findCandidates, computeScore } from '../../lib/catalog'
 
 describe('isObsolete', () => {
   test('return false if catalog is not defined', () => {
@@ -116,5 +118,110 @@ describe('findCandidates', () => {
     const candidates = findCandidates(catalogs, [1])
     expect(candidates.length).toBe(1)
     expect(candidates[0].id).toBe(2)
+  })
+})
+
+describe('computeScore', () => {
+  test('return 0 for a catalog with no metrics', () => {
+    expect(computeScore({})).toBe(0)
+  })
+
+  test('return 0 for a catalog with no data', () => {
+    const catalog = {
+      metrics: {
+        datasets: {
+          partitions: {
+            openness: { yes: 1 },
+            download: { yes: 1 }
+          }
+        }
+      }
+    }
+
+    expect(computeScore(catalog)).toBe(0)
+  })
+
+  test('return score for a perfect but old catalog', () => {
+    const catalog = {
+      metrics: {
+        datasets: {
+          totalCount: 42,
+          partitions: {
+            openness: { yes: 42 },
+            download: { yes: 42 }
+          }
+        }
+      }
+    }
+
+    expect(computeScore(catalog)).toBe(100 * 100 * 100)
+  })
+
+  test('return score for a perfect catalog that was updated in the last 6 months', () => {
+    const catalog = {
+      metrics: {
+        mostRecentRevisionDate: moment().subtract(2, 'months').toDate(),
+        datasets: {
+          totalCount: 42,
+          partitions: {
+            openness: { yes: 42 },
+            download: { yes: 42 }
+          }
+        }
+      }
+    }
+
+    expect(computeScore(catalog)).toBe(100 * 100 * 100 * 10)
+  })
+
+  test('return score for a perfect catalog that was updated in the last month', () => {
+    const catalog = {
+      metrics: {
+        mostRecentRevisionDate: moment().subtract(10, 'days').toDate(),
+        datasets: {
+          totalCount: 42,
+          partitions: {
+            openness: { yes: 42 },
+            download: { yes: 42 }
+          }
+        }
+      }
+    }
+
+    expect(computeScore(catalog)).toBe(100 * 100 * 100 * 100)
+  })
+
+  test('return score for a catalog that is not completely open', () => {
+    const catalog = {
+      metrics: {
+        mostRecentRevisionDate: moment().subtract(10, 'days').toDate(),
+        datasets: {
+          totalCount: 10,
+          partitions: {
+            openness: { yes: 5 },
+            download: { yes: 10 }
+          }
+        }
+      }
+    }
+
+    expect(computeScore(catalog)).toBe(50 * 100 * 100 * 100)
+  })
+
+  test('return score for a catalog that is not completely downloadable', () => {
+    const catalog = {
+      metrics: {
+        mostRecentRevisionDate: moment().subtract(10, 'days').toDate(),
+        datasets: {
+          totalCount: 10,
+          partitions: {
+            openness: { yes: 10 },
+            download: { yes: 5 }
+          }
+        }
+      }
+    }
+
+    expect(computeScore(catalog)).toBe(100 * 50 * 50 * 100)
   })
 })
