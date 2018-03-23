be782815ad2ca30558df1cb2ee02fbff0ba05ef8
Bertrand Marron <bertrand.marron@gmail.com>
2018-03-23 18:36:47 +0100

    Fix producer count on publication admin

--- components/publication/organization/source-producers.js
+++ components/publication/organization/source-producers.js
@@ -7,19 +7,20 @@ import Link from '../../link'
 
 const SourceProducers = ({organization}) => (
   <div>
-    {organization.producers ?
+    {organization.producers && organization.producers.length > 0 ? (
       <div>
-        <strong>{organization.producers && organization.producers.length > 0}</strong> producteurs sont associés à votre organisation
+        <strong>{organization.producers.length}</strong> producteurs sont associés à votre organisation
         <ul>
           {organization.producers.map(producer => (
             <li key={producer._id}>{producer._id}</li>
           ))}
         </ul>
-      </div> :
-      <div>
-        Aucun producteur n’est associé à votre organisation.
       </div>
-    }
+    ) : (
+      <p>
+        Aucun producteur n’est associé à votre organisation.
+      </p>
+    )}
 
     <Link prefetch href={`/publication/producers?oid=${organization._id}`} as={`/publication/${organization._id}/producers`}>
       <a>
