66ebad873e43af405ac780c4fe31f30f8ece44aa
Bertrand Marron <bertrand.marron@gmail.com>
2018-03-26 13:35:42 +0200

    Hoist attach-session HOC

--- components/hoc/attach-session.js
+++ components/hoc/attach-session.js
@@ -1,27 +1,62 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import hoist from 'hoist-non-react-statics'
 
 import {getSession, clearSession} from '../../lib/session'
 
-export default Page => class extends React.PureComponent {
-  static propTypes = {
-    ssr: PropTypes.bool.isRequired
-  }
+export default Page => {
+  const Extended = hoist(class extends React.Component {
+    static propTypes = {
+      ssr: PropTypes.bool.isRequired
+    }
 
-  static childContextTypes = {
-    session: PropTypes.shape({
-      auth: PropTypes.bool,
-      user: PropTypes.shape({
-        id: PropTypes.string.isRequired,
-        first_name: PropTypes.string.isRequired,
-        last_name: PropTypes.string.isRequired,
-        avatar_thumbnail: PropTypes.string.isRequired
-      }),
-      clear: PropTypes.func.isRequired
-    })
-  }
+    static childContextTypes = {
+      session: PropTypes.shape({
+        auth: PropTypes.bool,
+        user: PropTypes.shape({
+          id: PropTypes.string.isRequired,
+          first_name: PropTypes.string.isRequired,
+          last_name: PropTypes.string.isRequired,
+          avatar_thumbnail: PropTypes.string.isRequired
+        }),
+        clear: PropTypes.func.isRequired
+      })
+    }
 
-  static async getInitialProps(context) {
+    state = {}
+
+    getChildContext() {
+      const {session} = this.state
+
+      return {
+        session: session ? {
+          auth: session.auth,
+          user: session.user,
+          clear: clearSession
+        } : null
+      }
+    }
+
+    async componentDidMount() {
+      const {ssr} = this.props
+
+      const session = await getSession({
+        force: ssr
+      })
+
+      this.setState({
+        session
+      })
+    }
+
+    render() {
+      return (
+        <Page {...this.props} />
+      )
+    }
+  }, Page)
+
+  Extended.getInitialProps = async context => {
     const props = Page.getInitialProps ? await Page.getInitialProps(context) : {}
 
     return {
@@ -30,35 +65,5 @@ export default Page => class extends React.PureComponent {
     }
   }
 
-  state = {}
-
-  getChildContext() {
-    const {session} = this.state
-
-    return {
-      session: session ? {
-        auth: session.auth,
-        user: session.user,
-        clear: clearSession
-      } : null
-    }
-  }
-
-  async componentDidMount() {
-    const {ssr} = this.props
-
-    const session = await getSession({
-      force: ssr
-    })
-
-    this.setState({
-      session
-    })
-  }
-
-  render() {
-    return (
-      <Page {...this.props} />
-    )
-  }
+  return Extended
 }
