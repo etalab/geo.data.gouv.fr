e9d0001580fcd7178334071d6ea9040da9df49a9
Bertrand Marron <bertrand.marron@gmail.com>
2018-03-30 18:11:08 +0200

    Set default configuration for tests

--- jest.setup.js
+++ jest.setup.js
@@ -5,5 +5,12 @@ import 'mock-local-storage'
 
 import {configure} from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
+import {setConfig} from 'next/config'
 
 configure({adapter: new Adapter()})
+
+// Initialize Next.js configuration with empty values.
+setConfig({
+  publicRuntimeConfig: {},
+  secretRuntimeConfig: {}
+})
