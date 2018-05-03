be3ca8a0ab96460851161c286ee75a8231ae2b30
Bertrand Marron <bertrand.marron@gmail.com>
2018-05-03 11:07:05 +0200

    Stop using react-mapbox-gl

--- components/centered-map/index.js
+++ components/centered-map/index.js
@@ -1,15 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import mapboxGl, {Source, Layer} from 'react-mapbox-gl'
+import mapboxgl from 'mapbox-gl'
 import bbox from '@turf/bbox'
 import flip from '@turf/flip'
 
+import mapStyle from 'mapbox-gl/dist/mapbox-gl.css'
+
 import {isBboxFlipped, flipBbox} from '../../lib/geo/bbox'
 
-import Events from './events'
 import Feature from './feature'
-
-const mapStyle = 'https://openmaptiles.geo.data.gouv.fr/styles/osm-bright/style.json'
 
 class CenteredMap extends React.Component {
   static propTypes = {
@@ -30,10 +29,6 @@ class CenteredMap extends React.Component {
   constructor(props) {
     super(props)
 
-    this.MapComponent = mapboxGl({
-      interactive: !props.frozen
-    })
-
     // This is not in the state because we do not want to trigger
     // a re-render when we disable fitBounds.
     this.bbox = bbox(props.data)
@@ -48,28 +43,163 @@ class CenteredMap extends React.Component {
         this.bbox = flipBbox(this.bbox)
       }
     }
-  }
 
-  componentDidMount() {
-    this.bbox = null
-  }
+    this.handlers = []
 
-  getBounds = () => {
-    const {bbox} = this
-
-    if (bbox) {
-      return [
-        [bbox[0], bbox[1]],
-        [bbox[2], bbox[3]]
-      ]
+    if (!props.frozen) {
+      for (const layer of ['point', 'polygon-fill', 'line']) {
+        this.handlers.push({
+          event: 'mousemove',
+          layer,
+          handler: this.onMouseMove.bind(this, layer)
+        }, {
+          event: 'mouseleave',
+          layer,
+          handler: this.onMouseLeave.bind(this, layer)
+        })
+      }
     }
   }
 
-  onMouseEnter = (map, layer, event) => {
+  componentDidMount() {
+    const {frozen} = this.props
+
+    this.map = new mapboxgl.Map({
+      container: this.mapContainer,
+      style: 'https://openmaptiles.geo.data.gouv.fr/styles/osm-bright/style.json',
+      interactive: !frozen
+    })
+
+    this.map.fitBounds(this.bbox, {
+      padding: 30,
+      linear: true
+    })
+
+    this.map.once('load', this.onLoad)
+
+    for (const {event, layer, handler} of this.handlers) {
+      this.map.on(event, layer, handler)
+    }
+  }
+
+  componentWillUnmount() {
+    const {map} = this
+
+    for (const {event, layer, handler} of this.handlers) {
+      map.off(event, layer, handler)
+    }
+  }
+
+  onLoad = () => {
+    const {map} = this
+    const {data} = this.props
+
+    map.addSource('data', {
+      type: 'geojson',
+      data
+    })
+
+    map.addSource('hover', {
+      type: 'geojson',
+      data: {
+        type: 'FeatureCollection',
+        features: []
+      }
+    })
+
+    map.addLayer({
+      id: 'point',
+      type: 'circle',
+      source: 'data',
+      paint: {
+        'circle-radius': 5,
+        'circle-color': '#3099df',
+        'circle-opacity': 0.6
+      },
+      filter: ['==', '$type', 'Point']
+    })
+
+    map.addLayer({
+      id: 'point-hover',
+      type: 'circle',
+      source: 'hover',
+      paint: {
+        'circle-radius': 5,
+        'circle-color': '#2c3e50',
+        'circle-opacity': 0.8
+      },
+      filter: ['==', '$type', 'Point']
+    })
+
+    map.addLayer({
+      id: 'polygon-fill',
+      type: 'fill',
+      source: 'data',
+      paint: {
+        'fill-color': '#3099df',
+        'fill-opacity': 0.3
+      },
+      filter: ['==', '$type', 'Polygon']
+    })
+
+    map.addLayer({
+      id: 'polygon-fill-hover',
+      type: 'fill',
+      source: 'hover',
+      paint: {
+        'fill-color': '#2c3e50',
+        'fill-opacity': 0.3
+      },
+      filter: ['==', '$type', 'Polygon']
+    })
+
+    map.addLayer({
+      id: 'polygon-outline',
+      type: 'line',
+      source: 'data',
+      paint: {
+        'line-color': '#4790E5',
+        'line-width': 2
+      },
+      filter: ['==', '$type', 'Polygon']
+    })
+
+    map.addLayer({
+      id: 'line',
+      type: 'line',
+      source: 'data',
+      paint: {
+        'line-color': '#3099df',
+        'line-width': 5,
+        'line-opacity': 0.8
+      },
+      filter: ['==', '$type', 'LineString']
+    })
+
+    map.addLayer({
+      id: 'line-hover',
+      type: 'line',
+      source: 'hover',
+      paint: {
+        'line-color': '#2c3e50',
+        'line-width': 5,
+        'line-opacity': 0.8
+      },
+      filter: ['==', '$type', 'LineString']
+    })
+  }
+
+  onMouseMove = (layer, event) => {
+    const {map} = this
     const canvas = map.getCanvas()
     canvas.style.cursor = 'pointer'
 
     const [feature] = event.features
+
+    map.getSource('hover').setData({
+      type: 'FeatureCollection',
+      features: [feature]
+    })
 
     this.setState({
       highlight: {
@@ -79,142 +209,25 @@ class CenteredMap extends React.Component {
     })
   }
 
-  onMouseLeave = map => {
+  onMouseLeave = () => {
+    const {map} = this
     const canvas = map.getCanvas()
     canvas.style.cursor = ''
 
-    this.setState({
-      highlight: null
+    map.getSource('hover').setData({
+      type: 'FeatureCollection',
+      features: []
     })
   }
 
   render() {
-    const {MapComponent: Map} = this
-    const {data, frozen} = this.props
     const {highlight} = this.state
 
     return (
-      <Map
-        fitBounds={this.getBounds()}
-        fitBoundsOptions={{
-          padding: 30,
-          linear: true
-        }}
-        style={mapStyle} /* eslint-disable-line react/style-prop-object */
-        containerStyle={{
-          height: '100%',
-          width: '100%'
-        }}
-      >
-        <Source id='data' geoJsonSource={{
-          type: 'geojson',
-          data
-        }} />
-
-        {highlight && (
-          <Source id='highlight' geoJsonSource={{
-            type: 'geojson',
-            data: highlight.feature.geometry
-          }} />
-        )}
-
-        {/* Point */}
-        <Layer
-          id='point'
-          sourceId='data'
-          type='circle'
-          filter={['in', '$type', 'Point']}
-          paint={{
-            'circle-radius': 5,
-            'circle-color': '#3099df',
-            'circle-opacity': 0.6
-          }}
-        />
-
-        {!frozen && highlight && (
-          <Layer
-            id='point-hover'
-            sourceId='highlight'
-            type='circle'
-            filter={['in', '$type', 'Point']}
-            paint={{
-              'circle-radius': 5,
-              'circle-color': '#2c3e50',
-              'circle-opacity': 0.8
-            }}
-          />
-        )}
-
-        {/* Polygon */}
-        <Layer
-          id='polygon'
-          sourceId='data'
-          type='fill'
-          filter={['==', '$type', 'Polygon']}
-          paint={{
-            'fill-color': '#3099df',
-            'fill-opacity': 0.3
-          }}
-        />
-
-        <Layer
-          id='polygon-outline'
-          sourceId='data'
-          type='line'
-          filter={['==', '$type', 'Polygon']}
-          paint={{
-            'line-color': '#4790E5',
-            'line-width': 2
-          }}
-        />
-
-        {!frozen && highlight && (
-          <Layer
-            id='polygon-hover'
-            sourceId='highlight'
-            type='fill'
-            filter={['==', '$type', 'Polygon']}
-            paint={{
-              'fill-color': '#2c3e50',
-              'fill-opacity': 0.3
-            }}
-          />
-        )}
-
-        {/* LineString */}
-        <Layer
-          id='line'
-          sourceId='data'
-          type='line'
-          filter={['==', '$type', 'LineString']}
-          paint={{
-            'line-color': '#3099df',
-            'line-width': 5,
-            'line-opacity': 0.8
-          }}
-        />
-
-        {!frozen && highlight && (
-          <Layer
-            id='line-hover'
-            sourceId='highlight'
-            type='line'
-            filter={['==', '$type', 'LineString']}
-            paint={{
-              'line-color': '#2c3e50',
-              'line-width': 5,
-              'line-opacity': 0.8
-            }}
-          />
-        )}
-
-        {!frozen && (
-          <Events
-            layers={['point', 'polygon', 'line']}
-            onMouseEnter={this.onMouseEnter}
-            onMouseLeave={this.onMouseLeave}
-          />
-        )}
+      <div className='container'>
+        <div ref={el => {
+          this.mapContainer = el
+        }} className='container' />
 
         {highlight && (
           <div className='info'>
@@ -222,7 +235,16 @@ class CenteredMap extends React.Component {
           </div>
         )}
 
+        <style
+          dangerouslySetInnerHTML={{__html: mapStyle}} // eslint-disable-line react/no-danger
+        />
         <style jsx>{`
+          .container {
+            position: relative;
+            height: 100%;
+            width: 100%;
+          }
+
           .info {
             position: absolute;
             pointer-events: none;
@@ -232,7 +254,7 @@ class CenteredMap extends React.Component {
             overflow: hidden;
           }
         `}</style>
-      </Map>
+      </div>
     )
   }
 }
