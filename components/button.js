c7bf7b7826157b5c82e9ccd6173783640166dd13
Bertrand Marron <bertrand.marron@gmail.com>
2018-06-18 10:36:59 +0200

    Fix link buttons not working on Firefox
    Fix #639

--- components/button.js
+++ components/button.js
@@ -1,101 +1,99 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import css from 'styled-jsx/css'
+
+const style = css`
+  @import 'colors';
+
+  .wrapper {
+    display: inline-block;
+    border: 0 none;
+    border-radius: 2px;
+    padding: 5px 7px;
+    border: 1px solid transparent;
+    cursor: pointer;
+    -webkit-font-smoothing: antialiased;
+    color: inherit;
+
+    &:disabled {
+      background-color: $lightgrey;
+      color: $grey;
+      cursor: not-allowed;
+    }
+  }
+
+  .block {
+    display: block;
+    width: 100%;
+  }
+
+  .large {
+    font-size: 1.2em;
+    padding: 12px 16px;
+
+    a {
+      margin: -13px -17px;
+      padding: 13px 17px;
+    }
+  }
+
+  // Colors
+  .blue {
+    background-color: $blue;
+    border-color: $blue;
+    color: $white;
+
+    &:hover {
+      background-color: darken($blue, 10%);
+      border-color: darken($blue, 10%);
+    }
+  }
+
+  .red {
+    background-color: $red;
+    border-color: $red;
+    color: $white;
+
+    &:hover {
+      background-color: darken($red, 10%);
+      border-color: darken($red, 10%);
+    }
+  }
+
+  .white {
+    background-color: $white;
+    border-color: $blue;
+    color: $blue;
+
+    &:hover {
+      background-color: lighten($blue, 43%);
+    }
+  }
+`
 
 const Button = ({children, type, size, color, href, disabled, block, ...props}) => {
-  const buttonProps = href ? {} : props
+  if (href) {
+    return (
+      <a
+        href={href}
+        className={`wrapper ${size} ${disabled ? '' : color} ${block ? 'block' : ''}`}
+        {...props}
+      >
+        {children}
+        <style jsx>{style}</style>
+      </a>
+    )
+  }
 
   return (
-    // Button does have a type, but eslint is broken.
-    // eslint-disable-next-line react/button-has-type
     <button
       type={type}
       disabled={disabled}
-      className={`${size} ${disabled ? '' : color} ${block ? 'block' : ''}`}
-      tabIndex={href ? -1 : null}
-      {...buttonProps}
+      className={`wrapper ${size} ${disabled ? '' : color} ${block ? 'block' : ''}`}
+      {...props}
     >
-      {href ? (
-        <a href={href} {...props}>{children}</a>
-      ) : children}
-
-      <style jsx>{`
-        @import 'colors';
-
-        button {
-          display: inline-block;
-          border: 0 none;
-          border-radius: 2px;
-          padding: 5px 7px;
-          border: 1px solid transparent;
-          cursor: pointer;
-          -webkit-font-smoothing: antialiased;
-
-          &:disabled {
-            background-color: $lightgrey;
-            color: $grey;
-            cursor: not-allowed;
-          }
-        }
-
-        a {
-          display: block;
-          margin: -6px -8px;
-          padding: 6px 8px;
-          color: inherit;
-
-          &:hover {
-            color: inherit;
-          }
-        }
-
-        .block {
-          display: block;
-          width: 100%;
-        }
-
-        .large {
-          font-size: 1.2em;
-          padding: 12px 16px;
-
-          a {
-            margin: -13px -17px;
-            padding: 13px 17px;
-          }
-        }
-
-        // Colors
-        .blue {
-          background-color: $blue;
-          border-color: $blue;
-          color: $white;
-
-          &:hover {
-            background-color: darken($blue, 10%);
-            border-color: darken($blue, 10%);
-          }
-        }
-
-        .red {
-          background-color: $red;
-          border-color: $red;
-          color: $white;
-
-          &:hover {
-            background-color: darken($red, 10%);
-            border-color: darken($red, 10%);
-          }
-        }
-
-        .white {
-          background-color: $white;
-          border-color: $blue;
-          color: $blue;
-
-          &:hover {
-            background-color: lighten($blue, 43%);
-          }
-        }
-      `}</style>
+      {children}
+      <style jsx>{style}</style>
     </button>
   )
 }
