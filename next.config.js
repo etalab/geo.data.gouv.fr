f7895bb37680d863c6e9f2f7224ca2aed5c1b103
Bertrand Marron <bertrand.marron@gmail.com>
2018-08-27 12:45:59 +0200

    Move alias in client/prod only

--- next.config.js
+++ next.config.js
@@ -50,18 +50,15 @@ module.exports = withConfig({
         reportFilename: join(__dirname, 'reports/bundles.html'),
         defaultSizes: 'gzip'
       }))
-    }
 
-    const chunkFilename = dev ? '[name]' : '[name].[contenthash]'
-    config.output.chunkFilename = isServer ? `${chunkFilename}.js` : `static/chunks/${chunkFilename}.js`
-
-    config.resolve.alias = {
-      // Replace lodash with lodash-es on client side.
-      // This reduces the bundles sizes greatly and allows for webpack
-      // scope hoisting in other dependencies.
-      // Both lodash and lodash-es should be installed, as lodash-es
-      // will not work server-side.
-      lodash: 'lodash-es'
+      config.resolve.alias = {
+        // Replace lodash with lodash-es on client side.
+        // This reduces the bundles sizes greatly and allows for webpack
+        // scope hoisting in other dependencies.
+        // Both lodash and lodash-es should be installed, as lodash-es
+        // will not work server-side.
+        lodash: 'lodash-es'
+      }
     }
 
     return config
