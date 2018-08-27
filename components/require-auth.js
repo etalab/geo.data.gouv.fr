c7573ee794557e5b10dd176f8cda7ce7b9354d32
Bertrand Marron <bertrand.marron@gmail.com>
2018-08-27 13:06:32 +0200

    Fix data.gouv.fr register url

--- components/require-auth.js
+++ components/require-auth.js
@@ -58,7 +58,7 @@ class RequireAuth extends React.Component {
             <Button href={loginUrl}>
               {t('auth.login')}
             </Button>
-            <Button href='https://id.data.gouv.fr/register/'>
+            <Button href='https://www.data.gouv.fr/register'>
               {t('auth.register')}
             </Button>
           </div>
